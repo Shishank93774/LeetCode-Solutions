ce3941814008109abec68f66e3f22d19a93427ec
Shishank93774 <75724093+Shishank93774@users.noreply.github.com>
2023-01-25 11:31:12 +0530

    Time: 528 ms (24.66%), Space: 100.6 MB (62.05%) - LeetHub

--- 2359-find-closest-node-to-given-two-nodes/2359-find-closest-node-to-given-two-nodes.cpp
+++ 2359-find-closest-node-to-given-two-nodes/2359-find-closest-node-to-given-two-nodes.cpp
@@ -2,11 +2,6 @@ class Solution {
 public:
     int closestMeetingNode(vector<int>& edges, int node1, int node2) {
         int n = edges.size();
-        vector<int> adj[n];
-        for(int i = 0; i<n; i++){
-            int v = edges[i];
-            if(v != -1) adj[i].push_back(v);
-        }
         queue<int> qu;
         vector<int> dist1(n, -1);
         qu.push(node1);
@@ -17,7 +12,7 @@ public:
                 int f = qu.front(); qu.pop();
                 if(dist1[f] != -1) continue;
                 dist1[f] = d;
-                if(adj[f].size()) qu.push(adj[f][0]);
+                if(edges[f] != -1) qu.push(edges[f]);
             }
             d++;
         }
@@ -31,19 +26,12 @@ public:
                 int f = qu.front(); qu.pop();
                 if(dist2[f] != -1) continue;
                 dist2[f] = d;
-                if(adj[f].size()) qu.push(adj[f][0]);
+                if(edges[f] != -1) qu.push(edges[f]);
             }
             d++;
         }
         int mnDist = INT_MAX, ans = INT_MAX;
-        for(int i = n-1; i>=0; i--){
-            if(dist1[i] >=0 and dist2[i] >= 0){
-                if(mnDist >= max(dist1[i], dist2[i])){
-                    ans = i;
-                    mnDist =  max(dist1[i], dist2[i]);
-                }
-            }
-        }
+        for(int i = 0; i<n; i++) if(dist1[i] >=0 and dist2[i] >= 0 and mnDist > max(dist1[i], dist2[i])) ans = i, mnDist =  max(dist1[i], dist2[i]);
         return ans==INT_MAX?-1:ans;
     }
 };
