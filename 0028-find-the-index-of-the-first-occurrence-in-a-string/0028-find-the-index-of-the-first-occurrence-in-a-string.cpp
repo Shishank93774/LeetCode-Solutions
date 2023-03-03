5930897e874418d6e55f713a56ad0ea583345274
Shishank93774 <75724093+Shishank93774@users.noreply.github.com>
2023-03-03 09:31:51 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (29.81%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -1,14 +1,19 @@
+int n, m, i, ti, j;
 class Solution {
-public:
-    int strStr(string t, string str) {
-        int n = t.length(), m = str.length();
-        for(int i = 0; i<n; i++){
-            int ti = i;
-            int j = 0;
+    int bruteForce(const string &t, const string &str){
+        for(i = 0; i<n; i++){
+            ti = i;
+            j = 0;
             while(i<n and j<m and t[i++] == str[j++])
             if(j == m) return i-m;
             i = ti;
         }
         return -1;
+    }
+public:
+    int strStr(string t, string str) {
+        // return t.find(str);
+        n = t.length(), m = str.length();
+        return bruteForce(t, str);
     }
 };
