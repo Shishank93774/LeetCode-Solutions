32414c0cfce5f930ea9592ac66d42b4844ad22c8
Shishank93774 <75724093+Shishank93774@users.noreply.github.com>
2023-03-07 11:17:08 +0530

    Time: 158 ms (86.06%), Space: 55.9 MB (70.91%) - LeetHub

--- 2141-maximum-running-time-of-n-computers/2141-maximum-running-time-of-n-computers.cpp
+++ 2141-maximum-running-time-of-n-computers/2141-maximum-running-time-of-n-computers.cpp
@@ -2,11 +2,28 @@ class Solution {
 public:
     long long maxRunTime(int k, vector<int>& arr) {
         int n = arr.size();
-        long long tot = accumulate(arr.begin(), arr.end(), 0ll);
-        priority_queue<int> pqu(arr.begin(), arr.end());
-        while(!pqu.empty() and k > 0 and pqu.top() > tot / k){
-            tot -= pqu.top(); k--; pqu.pop();
+        // long long tot = accumulate(arr.begin(), arr.end(), 0ll);
+        // priority_queue<int> pqu(arr.begin(), arr.end());
+        // while(!pqu.empty() and k > 0 and pqu.top() > tot / k){
+        //     tot -= pqu.top(); k--; pqu.pop();
+        // }
+        // return tot/k;
+        long long l = 0, r = accumulate(arr.begin(), arr.end(), 0ll)/k;
+        long long ans = 0;
+        while(l <= r){
+            long long mid = l + (r - l)/2;
+            int i = 0;
+            long long timeTaken = 0;
+            while(i<n){
+                timeTaken += min(1ll*arr[i++], mid);
+            }
+            if(timeTaken >= k*mid){
+                l = mid + 1;
+                ans = mid;
+            }else{
+                r = mid - 1;
+            }
         }
-        return tot/k;
+        return ans;
     }
 };
