04de0640529c67af553b3981d04e76aa25c0a42a
Shishank93774 <75724093+Shishank93774@users.noreply.github.com>
2023-03-29 12:56:01 +0530

    Time: 25 ms (61.01%), Space: 9.1 MB (59.87%) - LeetHub

--- 1402-reducing-dishes/1402-reducing-dishes.cpp
+++ 1402-reducing-dishes/1402-reducing-dishes.cpp
@@ -1,16 +1,22 @@
 int dp[505][505];
 
 class Solution {
-    int rec(int i, int time, vector<int> &arr, int n){
-        if(i == n) return 0;
-        if(dp[i][time] != -1) return dp[i][time];
-        return dp[i][time] = max(arr[i]*time + rec(i+1, time+1, arr, n), rec(i+1, time, arr, n));
-    }
+    // int rec(int i, int time, vector<int> &arr, int n){
+    //     if(i == n) return 0;
+    //     if(dp[i][time] != -1) return dp[i][time];
+    //     return dp[i][time] = max(arr[i]*time + rec(i+1, time+1, arr, n), rec(i+1, time, arr, n));
+    // }
 public:
     int maxSatisfaction(vector<int>& arr) {
-        memset(dp, -1, sizeof dp);
         sort(arr.begin(), arr.end());
-        return rec(0, 1, arr, arr.size());
+        memset(dp, 0, sizeof dp);
+        for(int i = arr.size() - 1; i>=0; i--){
+            for(int time = 1; time<=arr.size(); time++) dp[i][time] = max(arr[i] * time + dp[i+1][time+1], dp[i+1][time]);
+        }
+        return dp[0][1];
+            
+        // memset(dp, -1, sizeof dp);
+        // return rec(0, 1, arr, arr.size());
         
         
         // int sum = 0, ans = 0;
