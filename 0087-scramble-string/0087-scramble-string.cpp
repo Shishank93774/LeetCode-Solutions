656e8923d32959f6f4150e584ee0a8e300c33587
Shishank93774 <75724093+Shishank93774@users.noreply.github.com>
2023-03-30 12:41:42 +0530

    Time: 3 ms (99.15%), Space: 12.6 MB (85.52%) - LeetHub

--- 0087-scramble-string/0087-scramble-string.cpp
+++ 0087-scramble-string/0087-scramble-string.cpp
@@ -1,22 +1,20 @@
+unordered_map<string, bool> mp;
 class Solution {
-    unordered_map<string, bool> mp;
-    bool rec(string s1, string s2, int n){
-        if(mp.find(s1 + "|" + s2) != mp.end()) return mp[s1 + "|" + s2];
-        if(s1 == s2) return mp[s1 + "|" + s2] = true;
-        for(int i = 1; i<n; i++){
-            bool b1 = rec(s1.substr(0, i), s2.substr(n-i), i) && rec(s1.substr(i), s2.substr(0, n-i), n-i);
-            if(b1) return mp[s1 + "|" + s2] = true;
-            bool b2 = rec(s1.substr(0, i), s2.substr(0, i), i) && rec(s1.substr(i), s2.substr(i), n-i);
-            if(b2) return mp[s1 + "|" + s2] = true;
-        }
-        return mp[s1 + "|" + s2] = false;
-    }
 public:
     bool isScramble(string s1, string s2) {
-        int cnt[26] = {0};
-        for(char ch: s1) cnt[ch-'a']++;
-        for(char ch: s2) cnt[ch-'a']--;
-        for(int i = 0; i<26; i++) if(cnt[i]) return false;
-        return rec(s1, s2, s1.size());
+        short cnt[26] = {0}, n = s1.length();
+        for(short i = 0; i<n; i++) cnt[s1[i] - 'a']++, cnt[s2[i] - 'a']--;
+        for(short i = 0; i<26; i++) if(cnt[i]) return false;
+        
+        string cat = s1 + "|" + s2;
+        if(mp.find(cat) != mp.end()) return mp[cat];
+        if(s1 == s2) return mp[cat] = true;
+        for(short i = 1; i<n; i++){
+            bool b1 = isScramble(s1.substr(0, i), s2.substr(n-i)) && isScramble(s1.substr(i), s2.substr(0, n-i));
+            if(b1) return mp[cat] = true;
+            bool b2 = isScramble(s1.substr(0, i), s2.substr(0, i)) && isScramble(s1.substr(i), s2.substr(i));
+            if(b2) return mp[cat] = true;
+        }
+        return mp[cat] = false;
     }
 };
