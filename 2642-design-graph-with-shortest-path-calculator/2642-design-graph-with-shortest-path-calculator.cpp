bdb14e565f478616e3d522dfe650ecd652cbecb2
Shishank Rawat <75724093+Shishank93774@users.noreply.github.com>
2023-04-16 13:34:21 +0530

    Time: 674 ms (100.00%), Space: 61.5 MB (100.00%) - LeetHub

--- 2642-design-graph-with-shortest-path-calculator/2642-design-graph-with-shortest-path-calculator.cpp
+++ 2642-design-graph-with-shortest-path-calculator/2642-design-graph-with-shortest-path-calculator.cpp
@@ -1,33 +1,30 @@
 class Graph {
-    vector<vector<pair<int, int> > > adj;
+    vector<vector<int> > gf;
     int V;
 public:
     Graph(int n, vector<vector<int>>& edges) {
         V = n;
-        adj.resize(n);
-        for(int i = 0; i<edges.size(); i++){
-            adj[edges[i][0]].push_back({edges[i][1], edges[i][2]});
-        }
+        gf = vector<vector<int> >(n, vector<int>(n, INT_MAX));
+        for(auto &e: edges)
+            gf[e[0]][e[1]] = e[2];
+        
+        for(int i = 0; i<n; i++) gf[i][i] = 0;
+        
+        for(int k = 0; k<n; k++)
+            for(int j = 0; j<n; j++)
+                for(int i = 0; i<n; i++)
+                    gf[i][j] = min(1ll*gf[i][j], 1ll*gf[i][k] + gf[k][j]);
+        
     }
     
-    void addEdge(vector<int> edge) {
-        adj[edge[0]].push_back({edge[1], edge[2]});
+    void addEdge(vector<int> e) {
+        for(int i = 0; i<V; i++)
+            for(int j = 0; j<V; j++)
+                gf[i][j] = min(1ll*gf[i][j], 1ll*gf[i][e[0]] + e[2] + gf[e[1]][j]);
     }
     
     int shortestPath(int node1, int node2) {
-        priority_queue<pair<int, int>, vector<pair<int, int> >, greater<pair<int, int> > > pqu;
-        pqu.push({0, node1});
-        vector<int> dist(V+1, INT_MAX);
-        while(!pqu.empty()){
-            auto pr = pqu.top(); pqu.pop();
-            if(pr.second == node2) return pr.first;
-            if(dist[pr.second] < pr.first) continue;
-            dist[pr.second] = pr.first;
-            for(auto nbrPr: adj[pr.second]){
-                if(1ll*nbrPr.second + pr.first < 1ll*dist[nbrPr.first]) pqu.push({nbrPr.second + pr.first, nbrPr.first});
-            }
-        }
-        return -1;
+        return gf[node1][node2] == INT_MAX?-1:gf[node1][node2];        
     }
 };
 
