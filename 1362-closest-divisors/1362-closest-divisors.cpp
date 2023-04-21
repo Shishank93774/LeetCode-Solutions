0aa20b2a9d0d503939eebe67dd7f426e1abf9b8e
Shishank Rawat <75724093+Shishank93774@users.noreply.github.com>
2023-04-22 00:50:23 +0530

    Time: 2 ms (100.00%), Space: 6.2 MB (67.62%) - LeetHub

---
+++ 1362-closest-divisors/1362-closest-divisors.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    vector<int> closestDivisors(int num) {
+        for(int i = sqrt(num+2); i>=1; i--){
+            if((num+1)%i == 0) return {i, (num+1)/i};
+            if((num+2)%i == 0) return {i, (num+2)/i};
+        }
+        return {};
+    }
+};
