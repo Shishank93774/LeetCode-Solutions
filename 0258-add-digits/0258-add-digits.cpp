b37cb83961f7faa3eb19a6d81de677b54ca20aaf
Shishank Rawat <75724093+Shishank93774@users.noreply.github.com>
2023-04-26 17:49:33 +0530

    Time: 3 ms (56.14%), Space: 5.9 MB (63.10%) - LeetHub

---
+++ 0258-add-digits/0258-add-digits.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int addDigits(int num) {
+        if(num<10) return num;
+        int res = 0;
+        while(num > 0){
+            res += num%10;
+            num /= 10;
+        }
+        return addDigits(res);
+    }
+};
