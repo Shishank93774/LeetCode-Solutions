07b46759eb23c953edf5ca70f63a132c82f47f86
Shishank Rawat <75724093+Shishank93774@users.noreply.github.com>
2023-04-27 15:31:46 +0530

    Time: 3 ms (45.34%), Space: 5.9 MB (94.29%) - LeetHub

--- 0319-bulb-switcher/0319-bulb-switcher.cpp
+++ 0319-bulb-switcher/0319-bulb-switcher.cpp
@@ -1,6 +1,15 @@
 class Solution {
 public:
     int bulbSwitch(int n) {
-        return sqrt(n);
+        int l = 1, r = n;
+        while(l<=r){
+            int mid = l + (r - l)/2;
+            if(1ll*mid*mid <= n){
+                l = mid + 1;
+            }else{
+                r = mid - 1;
+            }
+        }
+        return l-1;
     }
 };
