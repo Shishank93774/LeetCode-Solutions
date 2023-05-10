976b0df8c77ea64a2a992fde4ac126d25f651fbf
Shishank Rawat <75724093+Shishank93774@users.noreply.github.com>
2023-05-10 16:32:18 +0530

    Time: 27 ms (82.25%), Space: 6.2 MB (95.21%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.cpp
+++ 0037-sudoku-solver/0037-sudoku-solver.cpp
@@ -1,26 +1,25 @@
-int N = 9;
-int starts[9][2] = {{0, 0}, {0, 3}, {0, 6},
+short N = 9;
+short starts[9][2] = {{0, 0}, {0, 3}, {0, 6},
                     {3, 0}, {3, 3}, {3, 6},
                     {6, 0}, {6, 3}, {6, 6}};
 
-int dir[9][2] = {{0, 0}, {0, 1}, {0, 2},
+short dir[9][2] = {{0, 0}, {0, 1}, {0, 2},
                  {1, 0}, {1, 1}, {1, 2},
                  {2, 0}, {2, 1}, {2, 2}};
 
 class Solution {
-    bool valid(int r, int c, char v, vector<vector<char> > &arr){
-        for(int j = 0; j<9; j++) if(arr[r][j] == v) return false;
-        for(int i = 0; i<9; i++) if(arr[i][c] == v) return false;
-        int br = r/3, bc = c/3;
-        int t = br*3 + bc;
-        int x = starts[t][0], y = starts[t][1];
-        for(int j = 0; j<9; j++){
-            int tx = x + dir[j][0], ty = y + dir[j][1];
+    bool valid(short r, short c, char v, vector<vector<char> > &arr){
+        for(short j = 0; j<9; j++) if(arr[r][j] == v or arr[j][c] == v) return false;
+        short br = r/3, bc = c/3;
+        short t = br*3 + bc;
+        short x = starts[t][0], y = starts[t][1];
+        for(short j = 0; j<9; j++){
+            short tx = x + dir[j][0], ty = y + dir[j][1];
             if(arr[tx][ty] == v) return false;
         }
         return true;
     }
-    bool rec(int r, int c, vector<vector<char> > &arr){
+    bool rec(short r, short c, vector<vector<char> > &arr){
         if(r == 9) return true;
         if(c == 9) return rec(r+1, 0, arr);
         if(arr[r][c] != '.') return rec(r, c+1, arr);
