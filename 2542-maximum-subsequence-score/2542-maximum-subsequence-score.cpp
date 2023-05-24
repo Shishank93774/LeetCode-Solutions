7be0db0c1fe6894fea7976f7aef0afa5df6862be
Shishank Rawat <75724093+Shishank93774@users.noreply.github.com>
2023-05-24 09:20:03 +0530

    Time: 299 ms (48.28%), Space: 89.1 MB (76.35%) - LeetHub

--- 2542-maximum-subsequence-score/2542-maximum-subsequence-score.cpp
+++ 2542-maximum-subsequence-score/2542-maximum-subsequence-score.cpp
@@ -9,19 +9,21 @@ public:
         vector<int> corr(n);
         for(int i = 0; i<n; i++) corr[i] = nums1[brr[i].second];
         
-        // priority_queue<int, vector<int>, greater<int> > pqu;
-        multiset<int> mst;
+        priority_queue<int, vector<int>, greater<int> > pqu;
+        // multiset<int> mst;
         
         long long suf = 0;
-        // for(int i = n-k; i<n; i++) pqu.push(corr[i]), suf += corr[i];
-        for(int i = n-k; i<n; i++) mst.insert(corr[i]), suf += corr[i];
+        for(int i = n-k; i<n; i++) pqu.push(corr[i]), suf += corr[i];
+        // for(int i = n-k; i<n; i++) mst.insert(corr[i]), suf += corr[i];
 
         long long ans = suf*brr[n-k].first;
         
         for(int i = n-k-1; i>=0; i--){
             suf += corr[i];
-            mst.insert(corr[i]);
-            suf -= *mst.begin(); mst.erase(mst.begin());
+            pqu.push(corr[i]);
+            // mst.insert(corr[i]);
+            suf -= pqu.top(); pqu.pop();
+            // suf -= *mst.begin(); mst.erase(mst.begin());
             ans = max(ans, suf*brr[i].first);
         }
         return ans;
