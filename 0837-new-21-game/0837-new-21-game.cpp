e3a45b0b95a6ff49d9f595cc7c0ceb257d4de094
Shishank Rawat <75724093+Shishank93774@users.noreply.github.com>
2023-05-25 17:06:35 +0530

    Time: 2 ms (83.06%), Space: 6.1 MB (98.39%) - LeetHub

---
+++ 0837-new-21-game/0837-new-21-game.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    double new21Game(int n, int k, int maxPts) {
+        if(k == 0 or k+maxPts <=n) return 1.0;
+        double windowSum = (n-k+1);
+        
+        double dp[k];
+        for(int i = k-1; i>=0; i--){
+            dp[i] = windowSum/maxPts;
+            double last = 0;
+            if(i+maxPts < k) last = dp[i+maxPts];
+            else if(i+maxPts <= n) last = 1.0;
+            else last = 0.0;
+            windowSum += dp[i] - last;
+        }
+        
+        return dp[0];
+    }
+};
