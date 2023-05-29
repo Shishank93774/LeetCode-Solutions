b7a34d3b955a0c188ce0bb8149476120cc8aab71
Shishank Rawat <75724093+Shishank93774@users.noreply.github.com>
2023-05-29 08:49:54 +0530

    Time: 63 ms (57.52%), Space: 32.9 MB (88.19%) - LeetHub

--- 1603-design-parking-system/1603-design-parking-system.cpp
+++ 1603-design-parking-system/1603-design-parking-system.cpp
@@ -1,16 +1,8 @@
 class ParkingSystem {
-    int caps[3], cur[3];
+    int rem[3];
 public:
-    ParkingSystem(int big, int medium, int small){
-        caps[0] = big, caps[1] = medium, caps[2] = small;
-        cur[0] = cur[1] = cur[2] = 0;
-    }
-    
-    bool addCar(int t) {
-        --t;
-        if(cur[t] + 1 <= caps[t]) return (++cur[t]);
-        return false;
-    }
+    ParkingSystem(int big, int medium, int small){rem[0] = big, rem[1] = medium, rem[2] = small;}
+    bool addCar(int t) { return rem[--t] > 0 ? rem[t]-- : 0;}
 };
 
 /**
