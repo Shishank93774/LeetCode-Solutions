e23bbcc3904701a75dd1fc5fd7017dafe9b05f69
Shishank Rawat <75724093+Shishank93774@users.noreply.github.com>
2023-06-05 11:25:23 +0530

    Time: 7 ms (90.62%), Space: 10.1 MB (63.27%) - LeetHub

--- 1232-check-if-it-is-a-straight-line/1232-check-if-it-is-a-straight-line.cpp
+++ 1232-check-if-it-is-a-straight-line/1232-check-if-it-is-a-straight-line.cpp
@@ -1,15 +1,11 @@
 class Solution {
 public:
-    bool checkStraightLine(vector<vector<int>>& c){
-        int n = c.size();
-        if(n == 2)return true;
-        int dx = c[1][0] - c[0][0];
-        int dy = c[1][1] - c[0][1];
-        int cx,cy;
-        for(int i=1;i<n-1;i++){
-          cx = c[i+1][0] - c[i][0];
-          cy = c[i+1][1] - c[i][1];
-          if(dy*cx != dx*cy)return false;
+    bool checkStraightLine(vector<vector<int>>& arr) {
+        int n = arr.size();
+        int x1 = arr[0][0], y1 = arr[0][1], x2 = arr[1][0], y2 = arr[1][1],x3,y3;
+        for(int i = 2; i<n; i++){
+            x3 = arr[i][0], y3 = arr[i][1];
+            if((y2 - y1)*(x3 - x2) != (y3-y2)*(x2-x1)) return false;
         }
         return true;
     }
