902416bd4bed3c990c7e66f023f5e02f99f2b1d2
Shishank Rawat <75724093+Shishank93774@users.noreply.github.com>
2023-06-09 08:20:38 +0530

    Time: 15 ms (75.46%), Space: 15.9 MB (72.43%) - LeetHub

---
+++ 0744-find-smallest-letter-greater-than-target/0744-find-smallest-letter-greater-than-target.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    char nextGreatestLetter(vector<char>& letters, char target) {
+        for(char &ch: letters)
+            if(ch > target) return ch;
+        return letters[0];
+    }
+};
