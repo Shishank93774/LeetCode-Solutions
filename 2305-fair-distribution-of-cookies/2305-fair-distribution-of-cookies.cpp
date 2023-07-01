ce2d8a90d2373c69da9fb0cffeca88bd5127caff
Shishank Rawat <75724093+Shishank93774@users.noreply.github.com>
2023-07-01 12:39:43 +0530

    Time: 991 ms (65.87%), Space: 6.8 MB (62.54%) - LeetHub

--- 2305-fair-distribution-of-cookies/2305-fair-distribution-of-cookies.cpp
+++ 2305-fair-distribution-of-cookies/2305-fair-distribution-of-cookies.cpp
@@ -1,13 +1,14 @@
 class Solution {
     int mx;
-    void rec(int i, vector<int> &arr, vector<int> &cur, int n, int k){
+    void rec(int i, vector<int> &arr, vector<int> &cur, int n, int k, int avg){
         if(i == n){
             mx = min(mx, *max_element(cur.begin(), cur.end()));
             return;
         }
         for(int j = 0; j<k; j++){
+            if(cur[j] > avg) continue; 
             cur[j] += arr[i];
-            rec(i+1, arr, cur, n, k);
+            rec(i+1, arr, cur, n, k, avg);
             cur[j] -= arr[i];
         }
     }
@@ -15,7 +16,8 @@ public:
     int distributeCookies(vector<int>& arr, int k) {
         mx = INT_MAX;
         vector<int> cur(k, 0);
-        rec(0, arr, cur, arr.size(), k);
+        int sum = accumulate(arr.begin(), arr.end(), 0);
+        rec(0, arr, cur, arr.size(), k, (sum + k - 1)/k);
         return mx;
     }
 };
