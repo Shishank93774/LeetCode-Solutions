f7c715c69e48ac3f2551c4dc9ddf8dc097176a9e
Shishank Rawat <75724093+Shishank93774@users.noreply.github.com>
2023-07-02 20:36:58 +0530

    Time: 851 ms (29.41%), Space: 182.7 MB (36.27%) - LeetHub

--- 1601-maximum-number-of-achievable-transfer-requests/1601-maximum-number-of-achievable-transfer-requests.cpp
+++ 1601-maximum-number-of-achievable-transfer-requests/1601-maximum-number-of-achievable-transfer-requests.cpp
@@ -3,7 +3,7 @@ public:
     int maximumRequests(int n, vector<vector<int>>& requests) {
         int m = requests.size();
         int ans = 0;
-        for(int mask = 0; mask<(1<<m); mask++){
+        for(int mask = (1<<m)-1; mask > 0; mask--){
             vector<int> deg(n, 0);
             int cnt = 0;
             for(int i = 0; i<m; i++){
@@ -15,6 +15,7 @@ public:
             }
             bool chk = true;
             for(int i = 0; i<n; i++) if(deg[i] != 0) {chk = false; break;}
+            // if(chk) return cnt;
             if(chk) ans = max(ans, cnt);
         }
         return ans;
