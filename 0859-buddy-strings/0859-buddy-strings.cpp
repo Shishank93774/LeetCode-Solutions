b3cb6d9222176db0b0323f9bd06ce7c9f470caff
Shishank Rawat <75724093+Shishank93774@users.noreply.github.com>
2023-07-03 17:10:39 +0530

    Time: 0 ms (100.00%), Space: 6.9 MB (52.51%) - LeetHub

--- 0859-buddy-strings/0859-buddy-strings.cpp
+++ 0859-buddy-strings/0859-buddy-strings.cpp
@@ -4,7 +4,7 @@ public:
         int n = str.size();
         if(n == 1 or n != (int)t.size()) return false;
         if(n == 2) return str[0] == t[1] and str[1] == t[0];
-        if(str == t and n != (int)set<char>(str.begin(), str.end()).size()) return true;
+        if(str == t and n != (int)unordered_set<char>(str.begin(), str.end()).size()) return true;
         int cnt = 0, p1 = -1, p2 = -1;
         for(int i = 0; i<n; i++){
             if(str[i] != t[i]){
