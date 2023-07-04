8931ed5cf558739a60c2a6559f315954ed15f4fc
Shishank Rawat <75724093+Shishank93774@users.noreply.github.com>
2023-07-04 11:51:07 +0530

    Time: 7 ms (82.86%), Space: 9.4 MB (72.62%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.cpp
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -14,20 +14,27 @@ public:
         // }
         // return nums.back();
         
-        int n=nums.size();
-        unsigned int ans=0,shift=1;
-        for(int i=0;i<32;++i)
-        {
-            int count=0;
-            for(auto ele: nums)
-            {
-                if(ele & shift)
-                    count+=1;
-            }
-            if(count%3 != 0)
-                ans += shift;
-            shift*=2;
+        // int n=nums.size();
+        // unsigned int ans=0,shift=1;
+        // for(int i=0;i<32;++i)
+        // {
+        //     int count=0;
+        //     for(auto ele: nums)
+        //     {
+        //         if(ele & shift)
+        //             count+=1;
+        //     }
+        //     if(count%3 != 0)
+        //         ans += shift;
+        //     shift*=2;
+        // }
+        // return ans;
+        
+        int ones = 0, twos = 0;
+        for(int x: nums){
+            ones = ((ones ^ x) & (~twos));
+            twos = ((twos ^ x) & (~ones));
         }
-        return ans;
+        return ones;
     }
 };
