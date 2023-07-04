84cec5dc45dc4ae7bb08e5c647e97400770135e6
Shishank Rawat <75724093+Shishank93774@users.noreply.github.com>
2023-07-04 11:18:44 +0530

    Time: 7 ms (82.86%), Space: 9.4 MB (98.52%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.cpp
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -6,12 +6,28 @@ public:
         // for(auto it: mp) if(it.second == 1) return it.first;
         // return -1;
         
-        sort(nums.begin(), nums.end());
-        int i = 1;
-        while(i<nums.size()){
-            if(nums[i-1] != nums[i]) return nums[i-1];
-            i += 3;
+        // sort(nums.begin(), nums.end());
+        // int i = 1;
+        // while(i<nums.size()){
+        //     if(nums[i-1] != nums[i]) return nums[i-1];
+        //     i += 3;
+        // }
+        // return nums.back();
+        
+        int n=nums.size();
+        unsigned int ans=0,shift=1;
+        for(int i=0;i<32;++i)
+        {
+            int count=0;
+            for(auto ele: nums)
+            {
+                if(ele & shift)
+                    count+=1;
+            }
+            if(count%3 != 0)
+                ans += shift;
+            shift*=2;
         }
-        return nums.back();
+        return ans;
     }
 };
