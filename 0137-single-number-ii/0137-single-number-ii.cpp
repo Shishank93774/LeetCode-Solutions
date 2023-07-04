e40afdb60c39c658917f713f51a51bdd89537263
Shishank Rawat <75724093+Shishank93774@users.noreply.github.com>
2023-07-04 10:33:18 +0530

    Time: 4 ms (90.46%), Space: 9.4 MB (98.52%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.cpp
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -1,9 +1,17 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        map<int, int> mp;
-        for(int x: nums)mp[x]++;
-        for(auto it: mp) if(it.second == 1) return it.first;
-        return -1;
+        // map<int, int> mp;
+        // for(int x: nums)mp[x]++;
+        // for(auto it: mp) if(it.second == 1) return it.first;
+        // return -1;
+        
+        sort(nums.begin(), nums.end());
+        int i = 1;
+        while(i<nums.size()){
+            if(nums[i-1] != nums[i]) return nums[i-1];
+            i += 3;
+        }
+        return nums.back();
     }
 };
