b022cf61d9776a9826ecdc6f21cb27fa48fe1a0d
Shishank Rawat <75724093+Shishank93774@users.noreply.github.com>
2023-07-05 07:51:46 +0530

    Time: 41 ms (77.49%), Space: 36.4 MB (65.98%) - LeetHub

---
+++ 1493-longest-subarray-of-1s-after-deleting-one-element/1493-longest-subarray-of-1s-after-deleting-one-element.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int longestSubarray(vector<int>& arr) {
+        int ans = 0, n = arr.size(), i = 0, j = 0, has0 = 0;
+        while(j<n){
+            while(j<n and (has0+(arr[j] == 0))<2){
+                ans = max(ans, j-i);
+                has0 += (arr[j++] == 0);
+            }
+            has0 -= (arr[i++] == 0);
+        }
+        return ans;
+    }
+};
