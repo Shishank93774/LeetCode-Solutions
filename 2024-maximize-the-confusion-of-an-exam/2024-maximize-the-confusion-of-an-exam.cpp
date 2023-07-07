5b123caf0faf30f876a4080a3a77e3ce429be89b
Shishank Rawat <75724093+Shishank93774@users.noreply.github.com>
2023-07-07 14:49:35 +0530

    Time: 19 ms (97.34%), Space: 9 MB (100.00%) - LeetHub

--- 2024-maximize-the-confusion-of-an-exam/2024-maximize-the-confusion-of-an-exam.cpp
+++ 2024-maximize-the-confusion-of-an-exam/2024-maximize-the-confusion-of-an-exam.cpp
@@ -1,22 +1,37 @@
 class Solution {
 public:
     int maxConsecutiveAnswers(string &str, int k) {
-        int a1 = 0, a2 = 0, i = 0, j = 0, cur = 0, n = str.size();
+        int a1 = 0, a2 = 0, i = 0, j = 0, ans = 0, n = str.size();
         while(i<n and j<n){
-            while(j<n and cur + (str[j] == 'F') <=k){
-                cur += (str[j++] == 'F');
+            str[j]=='T'?a1++:a2++;
+            int mn = min(a1, a2);
+            if(mn <= k){
+                ans = max(ans, j-i+1);
+            }else{
+                while(mn > k){
+                    str[i++]=='T'?a1--:a2--;
+                    mn = min(a1, a2);
+                }
             }
-            a1 = max(a1, j-i);
-            cur -= (str[i++] == 'F');
+            j++;
         }
-        i = 0, j = 0, cur = 0;
-        while(i<n and j<n){
-            while(j<n and cur + (str[j] == 'T') <=k){
-                cur += (str[j++] == 'T');
-            }
-            a2 = max(a2, j-i);
-            cur -= (str[i++] == 'T');
-        }
-        return max(a1, a2);
+        return ans;
+        // int a1 = 0, a2 = 0, i = 0, j = 0, cur = 0, n = str.size();
+        // while(i<n and j<n){
+        //     while(j<n and cur + (str[j] == 'F') <=k){
+        //         cur += (str[j++] == 'F');
+        //     }
+        //     a1 = max(a1, j-i);
+        //     cur -= (str[i++] == 'F');
+        // }
+        // i = 0, j = 0, cur = 0;
+        // while(i<n and j<n){
+        //     while(j<n and cur + (str[j] == 'T') <=k){
+        //         cur += (str[j++] == 'T');
+        //     }
+        //     a2 = max(a2, j-i);
+        //     cur -= (str[i++] == 'T');
+        // }
+        // return max(a1, a2);
     }
 };
