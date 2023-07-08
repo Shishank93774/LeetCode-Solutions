e21b2c69758597f64a48ebc89bd74dbb91affe22
Shishank Rawat <75724093+Shishank93774@users.noreply.github.com>
2023-07-08 09:15:49 +0530

    Time: 193 ms (90.38%), Space: 59.9 MB (88.66%) - LeetHub

--- 2551-put-marbles-in-bags/2551-put-marbles-in-bags.cpp
+++ 2551-put-marbles-in-bags/2551-put-marbles-in-bags.cpp
@@ -1,22 +1,14 @@
 class Solution {
 public:
-    long long putMarbles(vector<int>& weights, int k) {
-        long long ans = 0;
-        int n = weights.size();
-        priority_queue<int> mxpqu;
-        priority_queue<int, vector<int>, greater<int> > mnpqu;
-        for(int i = 1; i<n; i++){
-            long long s = weights[i] + weights[i-1];
-            mnpqu.push(s);
-            mxpqu.push(s);
-            if(mnpqu.size() == k){
-                mnpqu.pop(); mxpqu.pop();
-            }
-        }
-        while(!mxpqu.empty()){
-            ans += mnpqu.top() - mxpqu.top();
-            mxpqu.pop(); mnpqu.pop();
-        }
-        return ans;
+    long long putMarbles(vector<int>& arr, int k) {
+        int n = arr.size();
+        if(n <= 2) return 0;
+        vector<int> give(n-1);
+        for(int i = 1; i<n; i++) give[i-1] = arr[i-1] + arr[i];
+        sort(give.begin(), give.end());
+        long long mn = accumulate(give.begin(), give.begin() + k - 1, 0ll);
+        reverse(give.begin(), give.end());
+        long long mx = accumulate(give.begin(), give.begin() + k - 1, 0ll);
+        return (mx - mn);
     }
 };
