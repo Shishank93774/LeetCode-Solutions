c1cbb92dc0aca625b3548983b53b01c2c1bb9e60
Shishank Rawat <75724093+Shishank93774@users.noreply.github.com>
2023-07-08 11:04:53 +0530

    Time: 2511 ms (5.23%), Space: 213.9 MB (7.19%) - LeetHub

--- 1755-closest-subsequence-sum/1755-closest-subsequence-sum.cpp
+++ 1755-closest-subsequence-sum/1755-closest-subsequence-sum.cpp
@@ -7,8 +7,7 @@ public:
         for(int i = 0; i<n/2; i++) fhalf.push_back(arr[i]);
         for(int i = n/2; i<n; i++) shalf.push_back(arr[i]);
         
-        set<int> fsubsets;
-        vector<int> ssubsets;
+        set<int> fsubsets, ssubsets;
         
         int sz = fhalf.size();
         fsubsets.insert(0);
@@ -23,7 +22,7 @@ public:
         }
         
         sz = shalf.size();
-        ssubsets.push_back(0);
+        ssubsets.insert(0);
         for(int mask = 1; mask<(1ll<<sz); mask++){
             int sum = 0;
             for(int j = 0; j<sz; j++){
@@ -31,15 +30,18 @@ public:
                     sum += shalf[j];
                 }
             }
-            ssubsets.push_back(sum);
+            ssubsets.insert(sum);
         }
-        sort(ssubsets.begin(), ssubsets.end());
         
         for(int x: fsubsets){
             int req = goal - x;
-            int idx = lower_bound(ssubsets.begin(), ssubsets.end(), req) - ssubsets.begin();
-            if(idx > 0) ans = min(ans, abs(goal - x - ssubsets[idx-1]));
-            if(idx<ssubsets.size()) ans = min(ans, abs(goal - x - ssubsets[idx]));
+            auto it = ssubsets.upper_bound(req);
+            if(it != ssubsets.end()) ans = min(ans, abs(req - *it));
+            if(it != ssubsets.begin()){
+                it--;
+                ans = min(ans, abs(req - *it));
+            }
+
         }
         return ans;
     }
