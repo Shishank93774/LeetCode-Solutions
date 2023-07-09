82b5489a18618d516cfda5dd4904dbff8e516f7c
Shishank Rawat <75724093+Shishank93774@users.noreply.github.com>
2023-07-09 11:50:25 +0530

    Time: 724 ms (6.29%), Space: 6.7 MB (100.00%) - LeetHub

---
+++ 2272-substring-with-largest-variance/2272-substring-with-largest-variance.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int largestVariance(string &str) {
+        int n = str.size();
+        int ans = 0;
+        for(char f = 'a'; f<='z'; f++){
+            for(char s = 'a'; s<='z'; s++){
+                for(int run = 1; run<=2; run++){
+                    int i = 0;
+                    int cntf = 0, cnts = 0;
+                    int res = 0;
+                    while(i<n){
+                        if(str[i] == f) cntf++;
+                        else if(str[i] == s) cnts++;
+                        if(cnts > cntf){
+                            cntf = cnts = 0;
+                        }
+                        res = cntf - cnts;
+                        if(cntf and cnts) ans = max(ans, res);
+                        i++;
+                    }
+                    reverse(str.begin(), str.end());
+                }
+            }
+        }
+        return ans;
+    }
+};
