f8dcd7ce7b2f20523b160e128830a455baf95c93
Shishank Rawat <75724093+Shishank93774@users.noreply.github.com>
2023-07-09 11:52:59 +0530

    Time: 440 ms (20.76%), Space: 6.6 MB (100.00%) - LeetHub

--- 2272-substring-with-largest-variance/2272-substring-with-largest-variance.cpp
+++ 2272-substring-with-largest-variance/2272-substring-with-largest-variance.cpp
@@ -6,21 +6,37 @@ public:
         for(char f = 'a'; f<='z'; f++){
             for(char s = 'a'; s<='z'; s++){
                 if(f == s) continue;
-                for(int run = 1; run<=2; run++){
-                    int i = 0;
-                    int cntf = 0, cnts = 0;
-                    int res = 0;
-                    while(i<n){
-                        if(str[i] == f) cntf++;
-                        else if(str[i] == s) cnts++;
-                        if(cnts > cntf){
-                            cntf = cnts = 0;
-                        }
-                        res = cntf - cnts;
-                        if(cntf and cnts) ans = max(ans, res);
-                        i++;
+                int i = 0;
+                int cntf = 0, cnts = 0;
+                int res = 0;
+                while(i<n){
+                    if(str[i] == f) cntf++;
+                    else if(str[i] == s) cnts++;
+                    if(cnts > cntf){
+                        cntf = cnts = 0;
                     }
-                    reverse(str.begin(), str.end());
+                    res = cntf - cnts;
+                    if(cntf and cnts) ans = max(ans, res);
+                    i++;
+                }
+            }
+        }
+        reverse(str.begin(), str.end());
+        for(char f = 'a'; f<='z'; f++){
+            for(char s = 'a'; s<='z'; s++){
+                if(f == s) continue;
+                int i = 0;
+                int cntf = 0, cnts = 0;
+                int res = 0;
+                while(i<n){
+                    if(str[i] == f) cntf++;
+                    else if(str[i] == s) cnts++;
+                    if(cnts > cntf){
+                        cntf = cnts = 0;
+                    }
+                    res = cntf - cnts;
+                    if(cntf and cnts) ans = max(ans, res);
+                    i++;
                 }
             }
         }
