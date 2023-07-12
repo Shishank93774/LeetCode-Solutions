46278846f8a2172be0f8b1c7a590fcabf13ee7d7
Shishank Rawat <75724093+Shishank93774@users.noreply.github.com>
2023-07-12 13:09:09 +0530

    Time: 230 ms (34.37%), Space: 61.5 MB (22.44%) - LeetHub

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
@@ -14,16 +14,44 @@ class Solution {
         marker[src] = safe;
         return safe;
     }
+    
+    void dfs2(int src, vector<vector<int> > &adj, vector<int> &indeg, vector<bool> &vis, vector<int> &safeNodes){
+        if(vis[src]) return;
+        vis[src] = true;
+        safeNodes.push_back(src);
+        for(int nbr: adj[src]){
+            indeg[nbr]--;
+            if(indeg[nbr] == 0) dfs2(nbr, adj, indeg, vis, safeNodes);
+        }
+    }
+    
 public:
     vector<int> eventualSafeNodes(vector<vector<int>>& adj) {
         vector<int> safeNodes;
         int n = adj.size();
-        vector<bool> marker(n, false);
-        vector<bool> vis(n, false);
-        for(int src = 0; src<n; src++){
-            if(!vis[src]) dfs(src, -1, adj, vis, marker);
-            if(marker[src]) safeNodes.push_back(src);
+        
+        vector<vector<int> > revAdj(n);
+        vector<int> indeg(n, 0);
+        for(int i = 0; i<n; i++){
+            for(int j = 0; j<adj[i].size(); j++){
+                revAdj[adj[i][j]].push_back(i);
+                indeg[i]++;
+            }
         }
+        vector<bool> vis(n, false);
+       for(int i = 0; i<n; i++){
+            if(indeg[i] == 0){
+                dfs2(i, revAdj, indeg, vis, safeNodes);
+            }
+        }
+        sort(safeNodes.begin(), safeNodes.end());
         return safeNodes;
+        
+        // vector<bool> marker(n, false);
+        // for(int src = 0; src<n; src++){
+        //     if(!vis[src]) dfs(src, -1, adj, vis, marker);
+        //     if(marker[src]) safeNodes.push_back(src);
+        // }
+        // return safeNodes;
     }
 };
