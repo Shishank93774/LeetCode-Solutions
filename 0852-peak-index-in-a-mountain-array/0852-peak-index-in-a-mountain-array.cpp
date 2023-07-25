54ed088628f4a7ab16c4b7128b294107f2d1e37c
Shishank Rawat <75724093+Shishank93774@users.noreply.github.com>
2023-07-25 11:15:42 +0530

    Time: 121 ms (77.28%), Space: 59.7 MB (40.39%) - LeetHub

--- 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.cpp
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.cpp
@@ -5,13 +5,9 @@ public:
         int l = 1, r = n-2;
         while(l<=r){
             int mid = l + (r - l)/2;
-            if(mid-1>=0 and mid+1<n and arr[mid-1] < arr[mid] and arr[mid] > arr[mid+1]){
-                return mid;
-            }else if(mid+1<n and arr[mid] < arr[mid+1]){
-                l = mid + 1;
-            }else{
-                r = mid - 1;
-            }
+            if(mid-1>=0 and mid+1<n and arr[mid-1] < arr[mid] and arr[mid] > arr[mid+1]) return mid;
+            else if(mid+1<n and arr[mid] < arr[mid+1]) l = mid + 1;
+            else r = mid - 1;
         }
         return -1;
     }
