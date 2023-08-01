0964e8c5849241c1cb2b0e8d04d15905a1833e22
Shishank Rawat <75724093+Shishank93774@users.noreply.github.com>
2023-08-01 21:13:28 +0530

    Time: 30 ms (97.53%), Space: 6.6 MB (99.51%) - LeetHub

--- 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -3,19 +3,20 @@ public:
     int minInsertions(string &str) {
         
         int n = str.size();
-        vector<vector<int> > dp(n, vector<int>(n, 1e9));
+        vector<int> down(n, 1e9), cur(n, 1e9);
         
         for(int i = n-1; i>=0; i--){
             for(int j = 0; j<n; j++){
-                if(i>=j) dp[i][j] = 0;
-                else if(i+1 == j) dp[i][j] = (str[i] != str[j]);
+                if(i>=j) cur[j] = 0;
+                else if(i+1 == j) cur[j] = (str[i] != str[j]);
                 else{
-                    if(str[i] == str[j]) dp[i][j] = (i+1<n and j-1>=0)?dp[i+1][j-1]:1e9;
-                    else dp[i][j] = 1 + min((i+1<n)?dp[i+1][j]:0, (j-1>=0)?dp[i][j-1]:0);
+                    if(str[i] == str[j]) cur[j] = (i+1<n and j-1>=0)?down[j-1]:1e9;
+                    else cur[j] = 1 + min((i+1<n)?down[j]:0, (j-1>=0)?cur[j-1]:0);
                 }
             }
+            down = cur;
         }
         
-        return dp[0][n-1];
+        return down[n-1];
     }
 };
