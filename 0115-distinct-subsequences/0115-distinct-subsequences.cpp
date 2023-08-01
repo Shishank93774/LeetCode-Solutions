bdd507178da93013eeeb5fb8b61b7d777214461a
Shishank Rawat <75724093+Shishank93774@users.noreply.github.com>
2023-08-02 01:32:06 +0530

    Time: 3 ms (99.91%), Space: 6.2 MB (99.16%) - LeetHub

---
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -0,0 +1,18 @@
+int cur[1001];
+class Solution {
+public:
+    
+    int numDistinct(string &str, string &sub) {
+        int n = str.size(), m = sub.size();
+        for(int j = 0; j<m; j++) cur[j] = 0;
+        cur[m] = 1;
+        for(int i = n-1; i>=0; i--){
+            for(int j = 0; j<=m; j++){
+                cur[j] = cur[j];
+                if(str[i] == sub[j]) cur[j] = 1ll*cur[j] + cur[j+1];
+            }
+        }
+    
+        return cur[0];
+    }
+};
