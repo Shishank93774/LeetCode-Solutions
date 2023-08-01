3660c614bf3cb6e1a8671bc5f857212cf96aaca8
Shishank Rawat <75724093+Shishank93774@users.noreply.github.com>
2023-08-01 21:05:01 +0530

    Time: 43 ms (87.99%), Space: 28.3 MB (68.35%) - LeetHub

--- 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -3,20 +3,19 @@ public:
     int minInsertions(string &str) {
         
         int n = str.size();
-        vector<vector<int> > dp(n, vector<int>(n, -1));
-        auto rec = [&](int i, int j, auto &&rec)->int{
-            if(i >= j) return 0;
-            if(i+1 == j) return (str[i] != str[j]);
-            
-            if(dp[i][j] != -1) return dp[i][j];
-            
-            if(str[i] == str[j]) return dp[i][j] = rec(i+1, j-1, rec);
-            
-            int a1 = 1 + rec(i+1, j, rec);
-            int a2 = 1 + rec(i, j-1, rec);
-            
-            return dp[i][j] = min(a1, a2);
-        };
-        return rec(0, n-1, rec);
+        vector<vector<int> > dp(n, vector<int>(n, 1e9));
+        
+        for(int i = n-1; i>=0; i--){
+            for(int j = 0; j<n; j++){
+                if(i>=j) dp[i][j] = 0;
+                else if(i+1 == j) dp[i][j] = (str[i] != str[j]);
+                else{
+                    if(str[i] == str[j]) dp[i][j] = (i+1<n and j-1>=0)?dp[i+1][j-1]:1e9;
+                    else dp[i][j] = 1 + min((i+1<n)?dp[i+1][j]:0, (j-1>=0)?dp[i][j-1]:0);
+                }
+            }
+        }
+        
+        return dp[0][n-1];
     }
 };
