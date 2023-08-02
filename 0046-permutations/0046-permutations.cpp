c510f3e64a5d1d08b7eaf9be0b67148a30cf6985
Shishank Rawat <75724093+Shishank93774@users.noreply.github.com>
2023-08-02 09:38:06 +0530

    Time: 0 ms (100.00%), Space: 7.9 MB (39.26%) - LeetHub

---
+++ 0046-permutations/0046-permutations.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<vector<int>> permute(vector<int>& arr) {
+        
+        vector<vector<int> > ans;
+        int n = arr.size();
+        auto rec = [&](int i, auto &&rec)->void{
+            if(i == n){
+                ans.push_back(arr);
+                return;
+            }
+            rec(i+1, rec);
+            for(int j = i+1; j<n; j++){
+                swap(arr[i], arr[j]);
+                rec(i+1, rec);
+                swap(arr[i], arr[j]);
+            }
+        };
+        
+        rec(0, rec);
+        return ans;
+    }
+};
