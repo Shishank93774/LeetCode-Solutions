7a0fc6f81342c51ca8672eb68097878bca3ca1f2
Shishank Rawat <75724093+Shishank93774@users.noreply.github.com>
2023-08-02 21:07:17 +0530

    Time: 668 ms (54.64%), Space: 283.5 MB (5.20%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,25 +1,24 @@
 class Solution {
+    int rec(int i, int remk, bool buyed, vector<int> &arr, int n, vector<vector<vector<int> > > &dp){
+        if(i == n) return 0;
+        if(remk == 0) return 0;
+        
+        if(dp[i][remk][buyed] != -1) return dp[i][remk][buyed];
+        
+        int a1 = rec(i+1, remk, buyed, arr, n, dp);
+        int a2 = 0;
+        if(buyed){
+            a2 = arr[i] + rec(i+1, remk-1, false, arr, n, dp);
+        }else{
+            a2 = -arr[i] + rec(i+1, remk, true, arr, n, dp);
+        }
+        
+        return dp[i][remk][buyed] = max(a1, a2);
+    }
 public:
     int maxProfit(vector<int>& arr) {
         int n = arr.size();
-        int dpl[n], dpr[n];
-        int mxp = 0;
-        int mn = arr[0];
-        for(int i = 1; i<n; i++){
-            mn = min(mn, arr[i]);
-            mxp = max(mxp, arr[i] - mn);
-            dpl[i] = mxp;
-        }
-        mxp = 0;
-        dpl[0] = dpr[n-1] = 0;
-        int mx = arr[n-1];
-        for(int i = n-2; i>=0; i--){
-            mx = max(mx, arr[i]);
-            mxp = max(mxp, mx - arr[i]);
-            dpr[i] = mxp;
-        }
-        int ans = 0;
-        for(int i = 0; i<n; i++) ans = max(ans, dpl[i] + dpr[i]);
-        return ans;
+        vector<vector<vector<int> > > dp(n, vector<vector<int> >(2+1, vector<int>(2, -1)));
+        return rec(0, 2, false, arr, n, dp);
     }
 };
