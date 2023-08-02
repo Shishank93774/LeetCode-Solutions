2b39759180ea3b96a5e382c185a26fd95d9b7761
Shishank Rawat <75724093+Shishank93774@users.noreply.github.com>
2023-08-02 22:29:37 +0530

    Time: 3 ms (86.67%), Space: 11.1 MB (97.73%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -1,23 +1,16 @@
 class Solution {
-    int rec(int i, bool buyed, vector<int> &arr, int n, vector<vector<int> > &dp){
-        if(i >= n) return 0;
-        
-        if(dp[i][buyed] != -1) return dp[i][buyed];
-        
-        int a1 = rec(i+1, buyed, arr, n, dp);
-        int a2 = 0;
-        if(buyed){
-            a2 = arr[i] + rec(i+2, false, arr, n, dp);
-        }else{
-            a2 = -arr[i] + rec(i+1, true, arr, n, dp);
-        }
-        
-        return dp[i][buyed] = max(a1, a2);
-    }
 public:
     int maxProfit(vector<int>& arr) {
         int n = arr.size();
-        vector<vector<int> > dp(n, vector<int>(2, -1));
-        return rec(0, false, arr, n, dp);
+        vector<int> down(2, 0), down2(2, 0), cur(2, 0);
+        
+        for(int i = n-1; i>=0; i--){
+            cur[0] = max(down[0], - arr[i] + down[1]);
+            cur[1] = max(down[1], arr[i] + down2[0]);
+            down2 = down;
+            down = cur;
+        }
+        
+        return down[0];
     }
 };
