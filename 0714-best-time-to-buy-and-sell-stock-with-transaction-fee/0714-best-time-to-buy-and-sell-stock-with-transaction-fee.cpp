ce9b548bb52fad7e5257cedcaa91ed90ef514422
Shishank Rawat <75724093+Shishank93774@users.noreply.github.com>
2023-08-02 22:35:16 +0530

    Time: 167 ms (45.01%), Space: 96.2 MB (6.74%) - LeetHub

--- 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
@@ -1,21 +1,22 @@
 class Solution {
+    int rec(int i, bool buyed, vector<int> &arr, int n, int fee, vector<vector<int>> &dp){
+        if(i == n) return 0;
+    
+        if(dp[i][buyed] != -1) return dp[i][buyed];
+        int a1 = rec(i+1, buyed, arr, n, fee, dp);
+        int a2 = 0;
+        if(buyed){
+            a2 = rec(i+1, false, arr, n, fee, dp) + arr[i] - fee;
+        }else{
+            a2 = rec(i+1, true, arr, n, fee, dp) - arr[i];
+        }
+    
+        return dp[i][buyed] = max(a1, a2);
+    }
 public:
-    int maxProfit(vector<int>& prices, int fee) {
-        int n = prices.size();
+    int maxProfit(vector<int>& arr, int fee) {
+        int n = arr.size();
         vector<vector<int> > dp(n, vector<int>(2, -1));
-        auto rec = [&](int i, bool buy, auto &&rec)->int {
-            if(i == n) return 0;
-
-            if(dp[i][buy] != -1) return dp[i][buy]; 
-            int &ans = dp[i][buy] = rec(i+1, buy, rec); // Do Nothing
-            if(buy) // Can Buy?
-                ans = max(ans, -prices[i] + rec(i+1, false, rec)); // Buy Now
-            else
-                ans = max(ans, prices[i] - fee + rec(i+1, true, rec)); // Sell Now
-            return ans;
-        };
-        
-        return rec(0, true, rec);
-        
+        return rec(0, false, arr, n, fee, dp);
     }
 };
