63beb9c9cb7a288863e1712e7d203db4a95b3359
Shishank Rawat <75724093+Shishank93774@users.noreply.github.com>
2023-08-02 22:05:58 +0530

    Time: 645 ms (54.70%), Space: 272.7 MB (5.20%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -5,15 +5,11 @@ public:
         vector<vector<vector<int> > > dp(n+1, vector<vector<int> >(2+1, vector<int>(2, 0)));
         
         for(int i = n-1; i>=0; i--){
-            for(int remk = 0; remk<=2; remk++){
-                if(i == n) dp[i][remk][0] = dp[i][remk][1] = 0;
-                else if(remk == 0) dp[i][remk][0] = dp[i][remk][1] = 0;
-                else{
-                    dp[i][remk][0] = dp[i+1][remk][0];
-                    dp[i][remk][1] = dp[i+1][remk][1];
-                    dp[i][remk][0] = max(dp[i][remk][0], - arr[i] + dp[i+1][remk][1]);
-                    dp[i][remk][1] = max(dp[i][remk][1], arr[i] + dp[i+1][remk-1][0]);
-                }
+            for(int remk = 1; remk<=2; remk++){
+                dp[i][remk][0] = dp[i+1][remk][0];
+                dp[i][remk][1] = dp[i+1][remk][1];
+                dp[i][remk][0] = max(dp[i][remk][0], - arr[i] + dp[i+1][remk][1]);
+                dp[i][remk][1] = max(dp[i][remk][1], arr[i] + dp[i+1][remk-1][0]);
             }
         }
         
