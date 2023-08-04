968eebbbcb7425b1391cbc8d268b6d488869b038
Shishank Rawat <75724093+Shishank93774@users.noreply.github.com>
2023-08-05 00:13:00 +0530

    Time: 0 ms (100.00%), Space: 6.7 MB (17.07%) - LeetHub

--- 0377-combination-sum-iv/0377-combination-sum-iv.cpp
+++ 0377-combination-sum-iv/0377-combination-sum-iv.cpp
@@ -1,23 +1,18 @@
+const long long mod = 1e18+7;
 class Solution {
 public:
     int combinationSum4(vector<int>& arr, int x) {
         
-        int n = arr.size();
-        
-        vector<int> dp(x+1, -1);
-        
-        auto rec = [&](int rem, auto &&rec)->int{
-            if(rem == 0) return 1;
+        int n = arr.size();     
+        vector<long long> dp(x+1, -1);        
+        dp[0] = 1;
+        for(int rem = 1; rem <= x; rem++){
+            long long ans = 0;
+            for(int i = 0; i<n; i++)
+                if(arr[i] <= rem) ans = (ans + dp[rem-arr[i]])%mod;
             
-            if(dp[rem] != -1) return dp[rem];
-            int ans = 0;
-            for(int i = 0; i<n; i++){
-                if(arr[i] <= rem) ans += rec(rem-arr[i], rec);
-            }
-            
-            return dp[rem] = ans;
-        };
-        
-        return rec(x, rec);
+            dp[rem] = ans;
+        }
+        return dp[x];
     }
 };
