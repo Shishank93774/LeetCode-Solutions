f5f123006db1b209b415f1bd28011df281f142b5
Shishank Rawat <75724093+Shishank93774@users.noreply.github.com>
2023-08-05 00:24:19 +0530

    Time: 298 ms (69.25%), Space: 9.7 MB (90.50%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -6,18 +6,19 @@ public:
         int sum = accumulate(arr.begin(), arr.end(), 0);
         if(sum%2) return false;
         
-        vector<vector<bool> > dp(n+1, vector<bool>(sum/2+1, false));
+        vector<bool> down(sum/2+1, false), cur(sum/2+1, false);
         
         for(int i = n-1; i>=0; i--){
             for(int rem = sum/2; rem>=0; rem--){
-                if(rem == 0) dp[i][rem] = true;
+                if(rem == 0) cur[rem] = true;
                 else{
-                    dp[i][rem] = dp[i+1][rem];
-                    if(arr[i] <= rem) dp[i][rem] = dp[i][rem] or dp[i+1][rem-arr[i]];
+                    cur[rem] = down[rem];
+                    if(arr[i] <= rem) cur[rem] = cur[rem] or down[rem-arr[i]];
                 }
             }
+            down = cur;
         }
         
-        return dp[0][sum/2];
+        return down[sum/2];
     }
 };
