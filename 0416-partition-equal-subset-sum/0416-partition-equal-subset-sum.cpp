5070b8eb946a1810d82420b08abfdc1d43ef375e
Shishank Rawat <75724093+Shishank93774@users.noreply.github.com>
2023-08-05 00:22:46 +0530

    Time: 460 ms (29.81%), Space: 13.2 MB (63.22%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -3,27 +3,21 @@ public:
     bool canPartition(vector<int> &arr) {
         
         int n = arr.size();
-        long long sum = accumulate(arr.begin(), arr.end(), 0ll);
-        
+        int sum = accumulate(arr.begin(), arr.end(), 0);
         if(sum%2) return false;
         
-        vector<vector<int> > dp(n, vector<int>(sum/2+1, -1));
+        vector<vector<bool> > dp(n+1, vector<bool>(sum/2+1, false));
         
-        auto rec = [&](int i, int rem, auto &rec)->bool{
-            if(rem == 0) return true;
-            if(i == n) return false;
-            
-            if(dp[i][rem] != -1) return dp[i][rem];
-            
-            int ans = 0;
-            ans = rec(i+1, rem, rec);
-            if(ans) return dp[i][rem] = true;
-            
-            if(arr[i] <= rem) ans = rec(i+1, rem-arr[i], rec);
-            
-            return dp[i][rem] = ans;
-        };
+        for(int i = n-1; i>=0; i--){
+            for(int rem = sum/2; rem>=0; rem--){
+                if(rem == 0) dp[i][rem] = true;
+                else{
+                    dp[i][rem] = dp[i+1][rem];
+                    if(arr[i] <= rem) dp[i][rem] = dp[i][rem] or dp[i+1][rem-arr[i]];
+                }
+            }
+        }
         
-        return rec(0, sum/2, rec);
+        return dp[0][sum/2];
     }
 };
