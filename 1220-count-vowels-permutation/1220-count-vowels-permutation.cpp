df2fa194d603e9aa547758350d6563af253009e2
Shishank Rawat <75724093+Shishank93774@users.noreply.github.com>
2023-08-06 12:20:55 +0530

    Time: 20 ms (70.59%), Space: 6.1 MB (78.35%) - LeetHub

--- 1220-count-vowels-permutation/1220-count-vowels-permutation.cpp
+++ 1220-count-vowels-permutation/1220-count-vowels-permutation.cpp
@@ -4,22 +4,23 @@ class Solution {
 public:
     int countVowelPermutation(int n) {
         
-        vector<vector<int> > dp(n+1, vector<int>(5, 0));
+        vector<int> down(5, 0), cur(5);
+        for(int p = 0; p<5; p++) down[p] = 1;
         
-        for(int p = 0; p<5; p++) dp[n][p] = 1;
-        for(int i = n-1; i>=0; i--){
+        for(int i = n-1; i>=1; i--){
             for(int p = 0; p<5; p++){
                 int ans = 0;
                 for(int nxt: nxtPos[p]){
-                    ans += dp[i+1][nxt];
+                    ans += down[nxt];
                     ans %= mod;
                 }
-                dp[i][p] = ans;
+                cur[p] = ans;
             }
+            down = cur;
         }
         int ans = 0;
         for(int p = 0; p<5; p++){
-            ans += dp[1][p];
+            ans += down[p];
             ans %= mod;
         }
         return ans;
