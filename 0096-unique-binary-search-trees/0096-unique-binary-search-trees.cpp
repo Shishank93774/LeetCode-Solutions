c6eff0f077dfb1a9003cc62a847b269695be6c80
Shishank Rawat <75724093+Shishank93774@users.noreply.github.com>
2023-08-07 12:11:49 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (11.11%) - LeetHub

--- 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
@@ -1,16 +1,21 @@
-const int N = 20;
-int catalan[N];
-
 class Solution {
 public:
     int numTrees(int n) {
-        catalan[0] = catalan[1] = 1;
-        for(int i = 2; i<=n; i++){
-            catalan[i] = 0;
-            for(int l = 0; l<i; l++){
-                catalan[i] += catalan[l]*catalan[i-1-l];
+        
+        vector<vector<int> >dp(n+1, vector<int>(n+1, -1));
+        auto rec = [&](int l, int r, auto &&rec)->int{
+            if(l > r) return 1;
+            
+            if(dp[l][r] != -1) return dp[l][r];
+            
+            int ans = 0;
+            for(int k = l; k<=r; k++){
+                ans += rec(l, k-1, rec)*rec(k+1, r, rec);
             }
-        }
-        return catalan[n];
+            
+            return dp[l][r] = ans;
+        };
+        
+        return rec(1, n, rec);
     }
 };
