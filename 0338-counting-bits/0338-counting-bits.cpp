ffe47d52934dab0bf9406cbbbb360991bda4f411
Shishank Rawat <75724093+Shishank93774@users.noreply.github.com>
2023-09-01 09:51:16 +0530

    Time: 0 ms (100.00%), Space: 7.8 MB (89.73%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -1,18 +1,13 @@
 class Solution {
-    bool isPowerOfTwo(int x){
-        return x && (!(x & (x - 1)));
-    }
 public:
     vector<int> countBits(int n) {
         if(n == 0) return {0};
-        if(n == 1) return {0, 1};
-        vector<int> dp(n+1);
-        dp[0] = 0;
+        vector<int> dp(n+1, 0);
         dp[1] = 1;
-        int msb = 0;
+        int msb = 1;
         for(int i = 2; i<=n; i++){
-            if(isPowerOfTwo(i)) msb++;
-            dp[i] = 1 + dp[i-(1<<msb)];
+            if(msb == (i>>1)) msb <<= 1;
+            dp[i] = 1 + dp[i-msb];
         }
         return dp;
     }
