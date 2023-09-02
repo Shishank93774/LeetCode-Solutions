e5d0cc7c4b1ff28e297812a2c7b9e0eacb51ed29
Shishank Rawat <75724093+Shishank93774@users.noreply.github.com>
2023-09-03 00:44:44 +0530

    Time: 14 ms (98.35%), Space: 6.6 MB (95.87%) - LeetHub

--- 2484-count-palindromic-subsequences/2484-count-palindromic-subsequences.cpp
+++ 2484-count-palindromic-subsequences/2484-count-palindromic-subsequences.cpp
@@ -1,33 +1,41 @@
-const int N = 1e4+4;
-const int MOD = 1e9+7;
-const int x = 5;
-typedef long long ll;
-ll dp[N][N];
-
+const int mod = 1e9+7;
 class Solution {
+    int rev(int n){
+        return (10*(n%10) + n/10);
+    }
 public:
-    int countPalindromes(string str) {
-        ll ans = 0;
-        memset(dp, sizeof(dp), 0);
-        int n = str.size();
-        for(int i = n-2; i>=0; i--){
-            for(int j = i+2; j<n; j++){
-                dp[i][j] = dp[i][j-1];
-                if(str[i] == str[j]){
-                    dp[i][j] = (dp[i][j] + j - i - 1)%MOD;
-                }
-                if(dp[i+1][j] != dp[i+1][j-1]){
-                    dp[i][j] = (dp[i][j] - dp[i+1][j-1] + dp[i+1][j])%MOD;
-                }
-            }
+    int countPalindromes(const string &str) {
+        int cntF[10] = {0}, cntB[10] = {0}, subF[100] = {0}, subB[100] = {0}, n = str.size();
+        for(int i = n-1; i>=0; i--){
+            int dig = str[i] - '0';
+            
+            for(int r = 0; r<=9; r++)
+                if(cntB[r])
+                    subB[dig*10+r] += cntB[r];
+            
+            cntB[dig]++;
         }
+        
+        long long ans = 0;
         
         for(int i = 0; i<n; i++){
-            for(int j = i + x - 1; j<n; j++){
-                if(str[i] == str[j]) ans = (ans + dp[i+1][j-1])%MOD;
+            int dig = str[i] - '0';
+            
+            cntB[dig]--;
+            for(int r = 0; r<=9; r++)
+                if(cntB[r])
+                    subB[dig*10+r] -= cntB[r];
+            
+            for(int sub = 0; sub<=99; sub++){
+                ans += 1ll*subF[sub]*subB[rev(sub)];
+                ans %= mod;
             }
+            
+            for(int l = 0; l<=9; l++)
+                if(cntF[l])
+                    subF[l*10+dig] += cntF[l];
+            cntF[dig]++;
         }
-        
         return ans;
     }
 };
