d1e259d37967d75bde875bbc7c307ca0e7ac9af0
Shishank Rawat <75724093+Shishank93774@users.noreply.github.com>
2023-09-13 11:06:28 +0530

    Time: 18 ms (53.51%), Space: 17.7 MB (72.88%) - LeetHub

---
+++ 0135-candy/0135-candy.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int candy(vector<int>& ratings) {
+        int n = ratings.size();
+        vector<int> candies(n, 1);
+        
+        for(int i = 1; i<n; i++){
+            if(ratings[i] > ratings[i-1]) candies[i] = candies[i-1] + 1;
+        }
+        
+        int ans = candies[n-1];
+        
+        for(int i = n-2; i>=0; i--){
+            if(ratings[i] > ratings[i+1]) candies[i] = max(candies[i+1]+1, candies[i]);
+            ans += candies[i];
+        }
+        
+        return ans;
+    }
+};
