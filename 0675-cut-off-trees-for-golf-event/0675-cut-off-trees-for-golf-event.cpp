c3ac4190c69884c08714e7ea233f3d0ef29083a2
Shishank Rawat <75724093+Shishank93774@users.noreply.github.com>
2023-09-18 12:43:20 +0530

    Time: 1168 ms (28.10%), Space: 161.3 MB (63.73%) - LeetHub

---
+++ 0675-cut-off-trees-for-golf-event/0675-cut-off-trees-for-golf-event.cpp
@@ -0,0 +1,57 @@
+int dxy[5] = {0, 1, 0, -1, 0};
+
+class Solution {
+    int nextPathDist(int sr, int sc, int dr, int dc, vector<vector<int> > &forest){
+        int n = forest.size(), m = forest[0].size();
+        
+        queue<pair<int, int> > qu;
+        qu.push({sr, sc});
+        vector<vector<bool> > vis(n, vector<bool>(m, false));
+        vis[sr][sc] = 1;
+        int d = 0;
+        while(!qu.empty()){
+            int sz = qu.size();
+            while(sz--){
+                auto [r, c] = qu.front(); qu.pop();
+                if(r == dr and c == dc) return d;
+                
+                for(int i = 0; i<4; i++){
+                    int tr = r + dxy[i], tc = c + dxy[i+1];
+                    if(tr<0 or tc<0 or tr>=n or tc>=m or forest[tr][tc] == 0 or vis[tr][tc]) continue;
+                    vis[tr][tc] = 1;
+                    qu.push({tr, tc});
+                }
+            }
+            d++;
+        }
+        
+        return -1;
+    }
+public:
+    int cutOffTree(vector<vector<int>>& forest) {
+        int n = forest.size(), m = forest[0].size();
+        
+        vector<vector<int> > visitingOrder;
+        for(int i = 0; i<n; i++){
+            for(int j = 0; j<m; j++){
+                if(forest[i][j] > 1) visitingOrder.push_back({forest[i][j], i, j});
+            }
+        }
+        sort(visitingOrder.begin(), visitingOrder.end());
+        int curRow = 0, curCol = 0;
+        int ans = 0;
+        
+        for(int i = 0; i<visitingOrder.size(); i++){
+            int nxtRow = visitingOrder[i][1], nxtCol = visitingOrder[i][2];
+            
+            int req = nextPathDist(curRow, curCol, nxtRow, nxtCol, forest);
+            
+            if(req == -1) return -1;
+            ans += req;
+            curRow = nxtRow, curCol = nxtCol;
+        }
+        
+        return ans;
+        
+    }
+};
