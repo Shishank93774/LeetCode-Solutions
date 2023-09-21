ac452b530d7e3bdc35262670b24cfb53eb8aea93
Shishank Rawat <75724093+Shishank93774@users.noreply.github.com>
2023-09-21 12:22:14 +0530

    Time: 27 ms (59.06%), Space: 89.6 MB (46.95%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,17 +1,18 @@
 class Solution {
-    double helper(vector<int> &a, vector<int> b){
+public:
+    double findMedianSortedArrays(vector<int>& a, vector<int>& b) {
         int n = a.size(), m = b.size();
+        if(n > m) return findMedianSortedArrays(b, a);
         bool odd = (n+m)%2;
         
-        int toLeft = (n+m+1)/2;
-        int l = 0, r = n;
+        int toLeft = (n+m+1)/2, l = 0, r = n, mid1, mid2, l1, l2, r1, r2;
         
         while(l<=r){
-            int mid1 = l + (r-l)/2;
-            int mid2 = toLeft - mid1;
+            mid1 = l + (r-l)/2;
+            mid2 = toLeft - mid1;
             
-            int l1 = INT_MIN, l2 = INT_MIN;
-            int r1 = INT_MAX, r2 = INT_MAX;
+            l1 = INT_MIN, l2 = INT_MIN;
+            r1 = INT_MAX, r2 = INT_MAX;
             
             if(mid1-1 >= 0) l1 = a[mid1-1];
             if(mid2-1 >= 0) l2 = b[mid2-1];
@@ -19,24 +20,13 @@ class Solution {
             if(mid2<m) r2 = b[mid2];
             
             if(l1 <= r2 and l2 <= r1){
-                if(odd){
-                    return max(l1, l2);
-                }
+                if(odd) return max(l1, l2);
                 return 1.0*(max(l1, l2) + min(r1, r2))/2.0;
             }
             if(l1 > r2) r = mid1 - 1;
             else l = mid1 + 1;
         }
         
-        return -1;
-    }
-public:
-    double findMedianSortedArrays(vector<int>& arr, vector<int>& brr) {
-        int n = arr.size();
-        int m = brr.size();
-        
-        if(n > m) return helper(brr, arr);
-        
-        return helper(arr, brr);   
+        return -1; 
     }
 };
