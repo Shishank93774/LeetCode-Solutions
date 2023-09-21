fcaedca414c7dac736239ea3525ccb3df9220ae7
Shishank Rawat <75724093+Shishank93774@users.noreply.github.com>
2023-09-21 12:18:58 +0530

    Time: 19 ms (89.46%), Space: 89.9 MB (32.23%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,85 +1,42 @@
 class Solution {
+    double helper(vector<int> &a, vector<int> b){
+        int n = a.size(), m = b.size();
+        bool odd = (n+m)%2;
+        
+        int toLeft = (n+m+1)/2;
+        int l = 0, r = n;
+        
+        while(l<=r){
+            int mid1 = l + (r-l)/2;
+            int mid2 = toLeft - mid1;
+            
+            int l1 = INT_MIN, l2 = INT_MIN;
+            int r1 = INT_MAX, r2 = INT_MAX;
+            
+            if(mid1-1 >= 0) l1 = a[mid1-1];
+            if(mid2-1 >= 0) l2 = b[mid2-1];
+            if(mid1<n) r1 = a[mid1];
+            if(mid2<m) r2 = b[mid2];
+            
+            if(l1 <= r2 and l2 <= r1){
+                if(odd){
+                    return max(l1, l2);
+                }
+                return 1.0*(max(l1, l2) + min(r1, r2))/2.0;
+            }
+            if(l1 > r2) r = mid1 - 1;
+            else l = mid1 + 1;
+        }
+        
+        return -1;
+    }
 public:
     double findMedianSortedArrays(vector<int>& arr, vector<int>& brr) {
         int n = arr.size();
         int m = brr.size();
-        bool odd = (n+m)%2;
-        int md = (n+m)/2;
-        if(odd){
-            int cnt = 0;
-            int i = 0, j = 0;
-            while(i<n and j<m){
-               if(arr[i]<=brr[j]){
-                   i++, cnt++;
-                   if(cnt > md){
-                       return arr[i-1];
-                   }
-               }else{
-                   j++, cnt++;   
-                   if(cnt > md){
-                      return brr[j-1];
-                   }
-               }
-            }
-            while(i<n){
-                i++, cnt++;
-                if(cnt > md){
-                       return arr[i-1];
-                }
-            }
-            while(j<m){
-                j++, cnt++;
-                if(cnt > md){
-                       return brr[j-1];
-                }
-            }
-        }else{
-            int cnt = 0;
-            int i = 0, j = 0;
-            int a1 = 0, a2 = 0;
-            while(i<n and j<m){
-               if(arr[i]<=brr[j]){
-                   i++, cnt++;
-                   if(cnt == md){
-                       a1 = arr[i-1];
-                   }
-                   if(cnt == md+1){
-                       a2 = arr[i-1];
-                       return (1.0*a1 + a2)/2.0;
-                   }
-               }else{
-                   j++, cnt++;   
-                   if(cnt == md){
-                       a1 = brr[j-1];
-                   }
-                   if(cnt == md+1){
-                       a2 = brr[j-1];
-                       return (1.0*a1 + a2)/2.0;
-                   }
-               }
-            }
-            while(i<n){
-                i++, cnt++;
-                if(cnt == md){
-                       a1 = arr[i-1];
-                   }
-                if(cnt == md+1){
-                      a2 = arr[i-1];
-                      return (1.0*a1 + a2)/2.0;
-                }
-            }
-            while(j<m){
-                j++, cnt++;
-                if(cnt == md){
-                       a1 = brr[j-1];
-                }
-                if(cnt == md+1){
-                       a2 = brr[j-1];
-                       return (1.0*a1 + a2)/2.0;
-                }
-            }
-            return (1.0*a1+a2)/2.0;
-        }
-        return 0;
+        
+        if(n > m) return helper(brr, arr);
+        
+        return helper(arr, brr);   
     }
 };
