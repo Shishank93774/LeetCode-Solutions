b409fc0733c0cb8a1be5e244c42aaf48fac24168
Shishank Rawat <75724093+Shishank93774@users.noreply.github.com>
2023-09-27 01:33:51 +0530

    Time: 4 ms (39.01%), Space: 6.7 MB (70.53%) - LeetHub

--- 0316-remove-duplicate-letters/0316-remove-duplicate-letters.cpp
+++ 0316-remove-duplicate-letters/0316-remove-duplicate-letters.cpp
@@ -1,25 +1,21 @@
 class Solution {
 public:
     string removeDuplicateLetters(string s) {
-        deque<char> qu;
         int cnt[26] = {};
         bool vis[26] = {};
         for(char &c: s) cnt[c-'a']++;
+        string ans;
         for(char &c: s){
-            if(vis[c-'a']) {cnt[c-'a']--; continue;}
-            while(!qu.empty() and qu.back() > c){
-                if(cnt[qu.back()-'a']){
-                    vis[qu.back() - 'a'] = false;
-                    qu.pop_back();
+            cnt[c-'a']--;
+            if(vis[c-'a']) continue;
+            while(!ans.empty() and ans.back() > c){
+                if(cnt[ans.back()-'a']){
+                    vis[ans.back()-'a'] = 0;
+                    ans.pop_back();
                 }else break;
             }
-            qu.push_back(c);
+            ans += c;
             vis[c-'a'] = true;
-            cnt[c-'a']--;
-        }
-        string ans;
-        while(!qu.empty()){
-            ans += qu.front(); qu.pop_front();
         }
         return ans;
     }
