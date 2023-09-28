53885e34f02d8f7a7e2ce97639aa6e5f72bd1c69
Shishank Rawat <75724093+Shishank93774@users.noreply.github.com>
2023-09-29 01:22:13 +0530

    Time: 8 ms (61.83%), Space: 9.4 MB (61.20%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.cpp
+++ 0047-permutations-ii/0047-permutations-ii.cpp
@@ -1,13 +1,14 @@
 class Solution {
-    void rec(int i, vector<int> &arr, int n, vector<vector<int> > &ans){
+    void rec(int i, vector<int> arr, int n, vector<vector<int> > &ans){
         if(i == n-1) {ans.push_back(arr); return;}
-        unordered_set<int> done;
+        // unordered_set<int> done;
         for(int j = i; j<n; j++){
-            if(done.count(arr[j])) continue;
-            done.insert(arr[j]);
+            // if(done.count(arr[j])) continue;
+            // done.insert(arr[j]);
+            if(j>i and arr[i] == arr[j]) continue;
             swap(arr[i], arr[j]);
             rec(i+1, arr, n, ans);
-            swap(arr[i], arr[j]);
+            // swap(arr[i], arr[j]);
         }
     }
 public:
