0598f9f9bbd2434f17458490baab30343275a2b6
Shishank Rawat <75724093+Shishank93774@users.noreply.github.com>
2023-09-28 10:50:50 +0530

    Time: 7 ms (66.47%), Space: 16.5 MB (25.63%) - LeetHub

--- 0905-sort-array-by-parity/0905-sort-array-by-parity.cpp
+++ 0905-sort-array-by-parity/0905-sort-array-by-parity.cpp
@@ -4,17 +4,14 @@ public:
         int l = 0, r = arr.size() - 1;
         while(l<r){
             if(arr[l]%2 and arr[r]%2){
-                r--;
-                swap(arr[l], arr[r]);
+                swap(arr[l], arr[--r]);
                 r--;
             }else if(arr[l]%2){
-                swap(arr[l], arr[r]);
-                l++; r--;
+                swap(arr[l++], arr[r--]);
             }else if(arr[r]%2){
                 l++; r--;
             }else{
-                l++;
-                swap(arr[l], arr[r]);
+                swap(arr[++l], arr[r]);
                 l++;
             }
         }
