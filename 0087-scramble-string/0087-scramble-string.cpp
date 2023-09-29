2a5860d02b582f8fa4c616f950dc972784cef666
Shishank Rawat <75724093+Shishank93774@users.noreply.github.com>
2023-09-29 15:20:05 +0530

    Time: 11 ms (91.03%), Space: 17.2 MB (69.79%) - LeetHub

--- 0087-scramble-string/0087-scramble-string.cpp
+++ 0087-scramble-string/0087-scramble-string.cpp
@@ -1,20 +1,21 @@
-unordered_map<string, bool> mp;
+unordered_map<string, bool> dp;
 class Solution {
 public:
     bool isScramble(string s1, string s2) {
-        short cnt[26] = {0}, n = s1.length();
-        for(short i = 0; i<n; i++) cnt[s1[i] - 'a']++, cnt[s2[i] - 'a']--;
-        for(short i = 0; i<26; i++) if(cnt[i]) return false;
+        if(s1 == s2) return true;
+        int n = s1.size();
+
+        vector<int> cnt(26, 0);
         
-        string cat = s1 + s2;
-        if(mp.find(cat) != mp.end()) return mp[cat];
-        if(s1 == s2) return mp[cat] = true;
-        for(short i = 1; i<n; i++){
-            bool b1 = isScramble(s1.substr(0, i), s2.substr(n-i)) && isScramble(s1.substr(i), s2.substr(0, n-i));
-            if(b1) return mp[cat] = true;
-            bool b2 = isScramble(s1.substr(0, i), s2.substr(0, i)) && isScramble(s1.substr(i), s2.substr(i));
-            if(b2) return mp[cat] = true;
+        for(int i = 0; i<n; i++) cnt[s1[i] - 'a']++, cnt[s2[i] - 'a']--;
+        for(int i = 0; i<26; i++) if(cnt[i]) return false;
+        
+        string comb = s1 + s2;
+        if(dp.find(comb) != dp.end()) return dp[comb];
+        for(int i = 1; i<n; i++){
+            if(isScramble(s1.substr(0, i), s2.substr(n-i)) and isScramble(s1.substr(i), s2.substr(0, n-i))) return dp[comb] = true;
+            if(isScramble(s1.substr(0, i), s2.substr(0, i)) and isScramble(s1.substr(i), s2.substr(i))) return dp[comb] = true;
         }
-        return mp[cat] = false;
+        return dp[comb] = false;
     }
 };
