fb91b5faa573b4b0744577802774700d749d9ce0
Shishank Rawat <75724093+Shishank93774@users.noreply.github.com>
2023-10-19 10:47:16 +0530

    Time: 3 ms (47.11%), Space: 6.6 MB (39.46%) - LeetHub

--- 0844-backspace-string-compare/0844-backspace-string-compare.cpp
+++ 0844-backspace-string-compare/0844-backspace-string-compare.cpp
@@ -1,15 +1,25 @@
 class Solution {
 public:
     bool backspaceCompare(string s, string t) {
-        string word1, word2;
-        for(char c: s){
-            if(c == '#') {if(!word1.empty()) word1.pop_back();}
-            else word1 += c;
+        int i = s.size() - 1, j = t.size() - 1;
+        while(i>=0 or j>=0){
+            int skipI = 0, skipJ = 0;
+            while(i>=0){
+                if(s[i] == '#') skipI++;
+                else if(skipI) skipI--;
+                else break;
+                i--;
+            }
+            while(j>=0){
+                if(t[j] == '#') skipJ++;
+                else if(skipJ) skipJ--;
+                else break;
+                j--;
+            }
+            if(i>=0 and j>=0 and s[i] != t[j]) return false;
+            if((i>=0) != (j>=0)) return false;
+            i--; j--;
         }
-        for(char c: t){
-            if(c == '#') {if(!word2.empty()) word2.pop_back();}
-            else word2 += c;
-        }
-        return word1 == word2;
+        return true;
     }
 };
