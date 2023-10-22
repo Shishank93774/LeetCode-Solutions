959e080a6d866aef6ab8e2220d799f0b1e187c22
Shishank Rawat <75724093+Shishank93774@users.noreply.github.com>
2023-10-22 10:48:26 +0530

    Time: 135 ms (64.47%), Space: 89.7 MB (73.03%) - LeetHub

--- 1793-maximum-score-of-a-good-subarray/1793-maximum-score-of-a-good-subarray.cpp
+++ 1793-maximum-score-of-a-good-subarray/1793-maximum-score-of-a-good-subarray.cpp
@@ -2,31 +2,18 @@ class Solution {
 public:
     int maximumScore(vector<int>& arr, int k) {
         int n = arr.size();
-        vector<int> pse(n), nse(n);
         
-        stack<int> stk;
-        
-        for(int i = 0; i<n; i++){
-            while(!stk.empty() and arr[stk.top()] >= arr[i]) stk.pop();
-            if(stk.empty()) pse[i] = -1;
-            else pse[i] = stk.top();
-            stk.push(i);
-        }
-        
-        stk = stack<int>();
-        for(int i = n-1; i>=0; i--){
-            while(!stk.empty() and arr[stk.top()] >= arr[i]) stk.pop();
-            if(stk.empty()) nse[i] = n;
-            else nse[i] = stk.top();
-            stk.push(i);
-        }
-        
-        int ans = 0;
-        for(int i = 0; i<n; i++){
-            int p = pse[i], nn = nse[i];
-            if(p < k and k < nn){
-                ans = max(ans, (nn - p - 1)*arr[i]);
-            }
+        int l = k, r = k;
+        int ans = arr[k];
+        int mn = arr[k];
+        while(l>0 or r+1<n){
+            int left = (l-1>=0?arr[l-1]:0);
+            int right = (r+1<n?arr[r+1]:0);
+            
+            if(left <= right){ r++; mn = min(mn, right);}
+            else {l--; mn = min(mn, left);}
+            
+            ans = max(ans, mn*(r - l + 1));
         }
         
         return ans;
