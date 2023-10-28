baf664b2493ec0610bfe5aa4128536f336100082
Shishank Rawat <75724093+Shishank93774@users.noreply.github.com>
2023-10-28 10:46:26 +0530

    Time: 5 ms (81.28%), Space: 9.4 MB (69.44%) - LeetHub

--- 1220-count-vowels-permutation/1220-count-vowels-permutation.cpp
+++ 1220-count-vowels-permutation/1220-count-vowels-permutation.cpp
@@ -1,26 +1,19 @@
-vector<int> nxtPos[5] = {{1},{0,2},{0,1,3,4},{2,4},{0}};
-const int mod = 1e9+7;
+const int mod = 1e9 + 7, N = 20001;
+int dp[N][6];
+vector<int> adj[6] = {{1, 2, 3, 4, 5}, {2}, {1, 3}, {1, 2, 4, 5}, {3, 5}, {1}};
+bool done = false;
 class Solution {
 public:
-    int countVowelPermutation(int n) {
-        
-        vector<int> down(5, 0), cur(5);
-        for(int p = 0; p<5; p++) down[p] = 1;
-        
-        for(int i = n-1; i>=1; i--){
-            for(int p = 0; p<5; p++){
-                int ans = 0;
-                for(int nxt: nxtPos[p]){
-                    ans += down[nxt];
-                    ans %= mod;
-                }
-                cur[p] = ans;
-            }
-            down = cur;
+    int countVowelPermutation(int n, int cur = 0) {
+        if(n == 0) return 1;
+        if(!done){
+            memset(dp, -1, sizeof dp);
+            done = true;
         }
-        int ans = 0;
-        for(int p = 0; p<5; p++){
-            ans += down[p];
+        if(dp[n][cur] != -1) return dp[n][cur];
+        int &ans = dp[n][cur] = 0;
+        for(int nxt: adj[cur]){
+            ans += countVowelPermutation(n-1, nxt);
             ans %= mod;
         }
         return ans;
