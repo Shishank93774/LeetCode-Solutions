34aebad0cdf8dfdc78d85c231b3d099b1e70569e
Shishank Rawat <75724093+Shishank93774@users.noreply.github.com>
2023-11-05 09:31:20 +0530

    Time: 82 ms (75.93%), Space: 63.5 MB (40.28%) - LeetHub

--- 1535-find-the-winner-of-an-array-game/1535-find-the-winner-of-an-array-game.cpp
+++ 1535-find-the-winner-of-an-array-game/1535-find-the-winner-of-an-array-game.cpp
@@ -2,25 +2,15 @@ class Solution {
 public:
     int getWinner(vector<int>& arr, int k) {
         int n = arr.size();
-        stack<int> stk;
-        vector<int> nge(n);
-        for(int i = n-1; i>=0; i--){
-            while(!stk.empty() and arr[stk.top()] < arr[i]) stk.pop();
-            if(stk.empty()) nge[i] = n;
-            else nge[i] = stk.top();
-            stk.push(i);
-        }
-        
+        int cur = arr[0];
+        int win = 0;
         int mx = *max_element(arr.begin(), arr.end());
-        int i = 0;
-        while(i<n and arr[i] != mx){
-            if(i == 0 and nge[i] - i - 1 >= k){
-                return arr[i];
-            }else if(i != 0 and nge[i] - i >= k){
-                return arr[i];
-            }
-            i++;
+        for(int i = 1; i<n; i++){
+            int oppo = arr[i];
+            if(cur > oppo) win++;
+            else cur = oppo, win = 1;
+            if(win == k or cur == mx) return cur;
         }
-        return mx;
+        return -1;
     }
 };
