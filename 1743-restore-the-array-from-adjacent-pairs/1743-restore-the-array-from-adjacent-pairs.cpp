14f032b3b388da86d18a254e626d522a4f8a6f05
Shishank Rawat <75724093+Shishank93774@users.noreply.github.com>
2023-11-10 10:49:52 +0530

    Time: 514 ms (30.30%), Space: 181.1 MB (5.15%) - LeetHub

--- 1743-restore-the-array-from-adjacent-pairs/1743-restore-the-array-from-adjacent-pairs.cpp
+++ 1743-restore-the-array-from-adjacent-pairs/1743-restore-the-array-from-adjacent-pairs.cpp
@@ -3,37 +3,24 @@ public:
     vector<int> restoreArray(vector<vector<int>>& adjacentPairs) {
         
         unordered_map<int, vector<int> > adj;
-        unordered_map<int, int> deg;
         for(auto it: adjacentPairs){
             int a = it[0], b = it[1];
             adj[a].push_back(b);
             adj[b].push_back(a);
-            deg[a]++;
-            deg[b]++;
         }
-        
-        unordered_map<int, int> timer;
+        vector<int> ans;
+        unordered_set<int> vis;
         auto dfs = [&](int u, int t, auto &&dfs)->void{
-            timer[u] = t++;
-            for(int v: adj[u]){
-                if(timer.find(v) != timer.end()) continue;
-                dfs(v, t, dfs);
-            }
+            ans.push_back(u);
+            vis.insert(u);
+            for(int v: adj[u])
+                if(vis.find(v) == vis.end()) dfs(v, t, dfs);
         };
         
         int s = 0;
-        for(auto [v, f]: deg){
-            if(f == 1) s = v;
-        }
-        
+        for(auto [v, f]: adj) if(f.size() == 1) s =  v;
         dfs(s, 0, dfs);
-        vector<pair<int, int> > vec;
-        for(auto [v, f]: deg){
-            vec.push_back({timer[v], v});
-        }
-        sort(vec.begin(), vec.end());
-        vector<int> ans;
-        for(auto [time, v]: vec) ans.push_back(v);
+        
         return ans;
     }
 };
