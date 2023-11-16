46dec7847e62e68bd1e531241feebcd2ae19227f
Shishank Rawat <75724093+Shishank93774@users.noreply.github.com>
2023-11-16 10:05:25 +0530

    Time: 0 ms (100.00%), Space: 10.7 MB (52.42%) - LeetHub

---
+++ 1980-find-unique-binary-string/1980-find-unique-binary-string.cpp
@@ -0,0 +1,30 @@
+bool vis[(1<<17)];
+
+class Solution {
+public:
+    string findDifferentBinaryString(vector<string>& nums) {
+        memset(vis, false, sizeof vis);
+        const int n = nums.size();
+        for(string s: nums){
+            int num = 0;
+            for(int i = n-1; i>=0; i--)
+                if(s[i] == '1') num += (1<<(n-1-i));
+            vis[num] = true;
+        }
+        
+        for(int i = 0; i<17; i++){
+            if(!vis[i]){
+                string ans;
+                while(i){
+                    if(i&1) ans += '1';
+                    else ans += '0';
+                    i >>= 1;
+                }
+                while(ans.size() < n) ans += '0';
+                reverse(ans.begin(), ans.end());
+                return ans;
+            }
+        }
+        return "-1";
+    }
+};
