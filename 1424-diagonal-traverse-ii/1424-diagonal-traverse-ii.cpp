0e82d6d2fffe31e82ba5bc37b0d1a227bb00fb82
Shishank Rawat <75724093+Shishank93774@users.noreply.github.com>
2023-11-22 08:54:11 +0530

    Time: 107 ms (97.65%), Space: 66 MB (94.66%) - LeetHub

--- 1424-diagonal-traverse-ii/1424-diagonal-traverse-ii.cpp
+++ 1424-diagonal-traverse-ii/1424-diagonal-traverse-ii.cpp
@@ -4,25 +4,14 @@ public:
         vector<int> ans;
         
         queue<pair<int, int> > qu;
-        
-        vector<vector<bool> > vis((int)nums.size());
-        for(int i = 0; i<nums.size(); i++) vis[i].assign((int)nums[i].size(), false);
-        
         qu.push({0, 0});
         while(!qu.empty()){
             auto [x, y] = qu.front(); qu.pop();
             ans.push_back(nums[x][y]);
             
-            if(x+1<nums.size() and y<nums[x+1].size() and !vis[x+1][y]){
-                vis[x+1][y] = true;
-                qu.push({x+1, y});
-            }
+            if(x+1<nums.size() and y==0) qu.push({x+1, y});
             
-            if(y+1<nums[x].size() and !vis[x][y+1]){
-                vis[x][y+1] = true;
-                qu.push({x, y+1});
-            }
-            
+            if(y+1<nums[x].size()) qu.push({x, y+1});
         }
         
         return ans;
