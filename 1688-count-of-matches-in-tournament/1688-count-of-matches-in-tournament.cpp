049baf787e4134a6dd37e3add4f06e635aa539d4
Shishank Rawat <75724093+Shishank93774@users.noreply.github.com>
2023-12-05 13:01:50 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (36.15%) - LeetHub

--- 1688-count-of-matches-in-tournament/1688-count-of-matches-in-tournament.cpp
+++ 1688-count-of-matches-in-tournament/1688-count-of-matches-in-tournament.cpp
@@ -4,6 +4,6 @@ public:
     int numberOfMatches(int n) {
         if(n <= 1) return 0;
         if(dp[n]) return dp[n];
-        return dp[n] = n%2?numberOfMatches(n-1)+1:numberOfMatches(n/2)+(n/2);
+        return dp[n] = n%2?numberOfMatches(n/2)+(n/2+1):numberOfMatches(n/2)+(n/2);
     }
 };
