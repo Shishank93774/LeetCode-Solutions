06d62e90f04fb25b263e10d69c6e6389d4680f1a
Shishank Rawat <75724093+Shishank93774@users.noreply.github.com>
2024-01-19 10:30:30 +0530

    Time: 7 ms (97.86%), Space: 10.5 MB (94.68%) - LeetHub

---
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int minFallingPathSum(vector<vector<int>>& arr) {
+        int n = arr.size(), m = arr[0].size();
+        int dp[n+1][m];
+        for(int j = 0; j<m; j++) dp[0][j] = 0;
+        int ans = 1e9;
+        for(int i = 1; i<=n; i++){
+            for(int j = 0; j<m; j++){
+                dp[i][j] = dp[i-1][j];
+                if(j-1 >= 0) dp[i][j] = min(dp[i][j], dp[i-1][j-1]);
+                if(j+1 < m) dp[i][j] = min(dp[i][j], dp[i-1][j+1]);
+                dp[i][j] += arr[i-1][j];
+                if(i == n) ans = min(dp[i][j], ans);
+            }
+        }
+        return ans;
+    }
+};
