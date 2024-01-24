0aac2e064fc9442319148ea427c2a5f0e8867e4f
Shishank Rawat <75724093+Shishank93774@users.noreply.github.com>
2024-01-24 10:56:34 +0530

    Time: 182 ms (99.73%), Space: 176.9 MB (99.46%) - LeetHub

--- 1457-pseudo-palindromic-paths-in-a-binary-tree/1457-pseudo-palindromic-paths-in-a-binary-tree.cpp
+++ 1457-pseudo-palindromic-paths-in-a-binary-tree/1457-pseudo-palindromic-paths-in-a-binary-tree.cpp
@@ -9,20 +9,28 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
+auto init = [](){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    return 'c';
+}();
+
 class Solution {
 public:
-    int pseudoPalindromicPaths (TreeNode* root, vector<int> cnt = vector<int>(10, 0)) {
-        cnt[root->val-1]++;
+    int pseudoPalindromicPaths (TreeNode* root, int mask = 0) {
+        mask ^= (1<<root->val);
         if(root->left == NULL and root->right == NULL){
             int cnto = 0;
-            for(int i = 0; i<10; i++){
-                cnto += (cnt[i]&1);
+            for(int i = 1; i<=10; i++){
+                cnto += ((mask>>i)&1);
             }
             return cnto <= 1;
         }
         int ans = 0;
-        if(root->left) ans += pseudoPalindromicPaths(root->left, cnt);
-        if(root->right) ans += pseudoPalindromicPaths(root->right, cnt);
+        if(root->left) ans += pseudoPalindromicPaths(root->left, mask);
+        if(root->right) ans += pseudoPalindromicPaths(root->right, mask);
         
         return ans;
     }
