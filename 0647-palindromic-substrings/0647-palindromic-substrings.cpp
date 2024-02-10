783fe9989c05383c29f0a4e651e80ec687c69883
Shishank Rawat <75724093+Shishank93774@users.noreply.github.com>
2024-02-10 13:16:35 +0530

    Time: 55 ms (18.58%), Space: 10.9 MB (54.86%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.cpp
+++ 0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -1,25 +1,32 @@
+auto init = [](){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    return 'c';
+}();
+
+vector<vector<bool> > vis, dp;
 class Solution {
-public:
-    int countSubstrings(string &str) {
-        int n = str.size();
-        vector<vector<int> > dp(n, vector<int>(n, -1));
-        auto rec = [&](int l, int r, auto &&rec)->bool{
-            if(l == r) return dp[l][r] = true;
-            if(l+1 == r) return dp[l][r] = (str[l] == str[r]);
-            
-            if(dp[l][r] != -1) return dp[l][r];
-            int ans = 0;
-            if(str[l] == str[r]){
-                return dp[l][r] = rec(l+1, r-1, rec);
-            }
-            return dp[l][r] = false;
-        };
-        int ans = 0;
-        for(int l = 0; l<n; l++){
-            for(int r = l; r<n; r++){
-                if(rec(l, r, rec)) ans++;
-            }
+    bool rec(int l, int r, const string &str){
+        if(l >= r) return dp[l][r] = true;
+        
+        if(vis[l][r]) return dp[l][r];
+        vis[l][r] = true;
+        if(str[l] == str[r]){
+            return dp[l][r] = rec(l+1, r-1, str);
         }
+        return dp[l][r] = false;
+    }
+public:
+    int countSubstrings(const string &str) {
+        int n = str.size(), ans = 0;
+        vis.assign(n, vector<bool>(n, false));
+        dp.assign(n, vector<bool>(n, false));
+        
+        for(int i = 0; i<n; i++)
+            for(int j = i; j<n; j++)
+                if(rec(i, j, str))
+                    ans++;
         
         return ans;
     }
