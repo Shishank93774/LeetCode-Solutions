f73cfde97172f0d9954614d0f4b28fa00586a3f7
Shishank Rawat <75724093+Shishank93774@users.noreply.github.com>
2024-05-13 03:00:03 +0530

    Time: 93 ms (100.00%), Space: 30.1 MB (100.00%) - LeetHub

--- 3149-find-the-minimum-cost-array-permutation/3149-find-the-minimum-cost-array-permutation.cpp
+++ 3149-find-the-minimum-cost-array-permutation/3149-find-the-minimum-cost-array-permutation.cpp
@@ -1,15 +1,15 @@
 const int N = 15;
-int dp[N][(1<<N)][N];
+int dp[N][(1<<N)];
 
 class Solution {
-    void genSol(int i, int last, int mask, int first, vector<int> &arr, vector<int> &sol){
+    void genSol(int i, int last, int mask, vector<int> &arr, vector<int> &sol){
         if(i == sol.size()) return;
         int mn = 1e9, v = -1;
         
         for(int b = 0; b<sol.size(); b++){
             if(mask&(1<<b)) continue;
             
-            int a = dp[b][mask|(1<<b)][first] + abs(last - arr[b]);
+            int a = dp[b][mask|(1<<b)] + abs(last - arr[b]);
             
             if(a< mn){
                 mn = a;
@@ -17,7 +17,7 @@ class Solution {
             }
         }
         sol[i] = v;
-        genSol(i+1, v, mask|(1<<v), first, arr, sol);
+        genSol(i+1, v, mask|(1<<v), arr, sol);
         
     }
 public:
@@ -27,41 +27,26 @@ public:
         
         for(int i = 0; i<n; i++)
             for(int b = 0; b<(1<<n); b++)
-                for(int j = 0; j<n; j++)
-                    dp[i][b][j] = -1;
+                dp[i][b] = -1;
         
         
-        auto rec = [&](int i, int last, int mask, int first, auto &&rec)->int{
-            if(i == n) return abs(last - arr[first]);
+        auto rec = [&](int i, int last, int mask, auto &&rec)->int{
+            if(i == n) return abs(last - arr[0]);
             
-            int &ans = dp[last][mask][first];
+            int &ans = dp[last][mask];
             if(ans != -1) return ans;
             
             ans = 1e9;
             
             for(int b = 0; b<n; b++){
                 if(mask&(1<<b)) continue;
-                int a = rec(i+1, b, mask|(1<<b), first, rec) + abs(last - arr[b]);
-                
-                if(a < ans){
-                    sol[i] = b;
-                }
-                
-                ans = min(ans, a);
+                ans = min(ans, rec(i+1, b, mask|(1<<b), rec) + abs(last - arr[b]));
             }
             return ans;
         };
-        int ans = 1e9;
-        int mn = 1e9, v = -1;
-        for(int f = 0; f<n; f++){
-            int a =  rec(1, f, 1<<f, f, rec);
-            if(a < mn){
-                mn = a;
-                v = f;
-            }
-        }
-        sol[0] = v;
-        genSol(1, v, 1<<v, v, arr, sol);
+        rec(1, 0, 1, rec);
+        sol[0] = 0;
+        genSol(1, 0, 1, arr, sol);
         
         return sol;
     }
