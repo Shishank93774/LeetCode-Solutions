2f41dea6869c18bf1c3e124f1719f4e042e44115
Shishank Rawat <75724093+Shishank93774@users.noreply.github.com>
2024-06-08 23:13:47 +0530

    Time: 700 ms (9.20%), Space: 174.3 MB (13.93%) - LeetHub

---
+++ 3160-find-the-number-of-distinct-colors-among-the-balls/3160-find-the-number-of-distinct-colors-among-the-balls.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> queryResults(int n, vector<vector<int>>& queries) {
+        vector<int> ans;
+        
+        map<int, int> colors;
+        map<int, int> balls;
+        for(auto v: queries){
+            int ball = v[0], color = v[1];
+            
+            if(balls.find(ball) != balls.end()){
+                int earlier_color = balls[ball];
+                if(--colors[earlier_color] == 0)
+                    colors.erase(earlier_color);
+            }
+            balls[ball] = color;
+            colors[color]++;
+
+            ans.push_back(colors.size());
+        }
+        
+        return ans;
+    }
+};
