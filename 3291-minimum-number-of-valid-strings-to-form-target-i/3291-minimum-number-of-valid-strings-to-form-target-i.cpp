a331998c76f76165a8d3f2efdb892c800db3f12e
Shishank Rawat <75724093+Shishank93774@users.noreply.github.com>
2024-09-16 13:26:50 +0530

    Time: 134 ms (100.00%), Space: 91.5 MB (23.08%) - LeetHub

--- 3291-minimum-number-of-valid-strings-to-form-target-i/3291-minimum-number-of-valid-strings-to-form-target-i.cpp
+++ 3291-minimum-number-of-valid-strings-to-form-target-i/3291-minimum-number-of-valid-strings-to-form-target-i.cpp
@@ -1,61 +1,90 @@
-struct TrieNode{
-    bool isEnd;
-    TrieNode* children[26];
-    TrieNode(){
-        isEnd = false;
-        for(int i = 0; i<26; i++) children[i] = NULL;
+struct segTree{
+    vector<int> seg;
+    vector<int> arr;
+    segTree(const vector<int> &a, int n){
+        arr = a;
+        seg.resize(4*n + 1);
     }
-};
-
-class Trie{
-public:
-    TrieNode *root;
-
-    Trie(){
-        root = new TrieNode();
-    }
-
-    void add(const string &str){
-        int n = str.size(), i = 0;
-
-        TrieNode *cur = root;        
-        while(i<n){
-            int idx = str[i] - 'a';
-            if(cur->children[idx] == NULL)
-                cur->children[idx] = new TrieNode();
-            cur = cur->children[idx];
-            i++;
+    void build(int idx, int low, int high){  // O(N)
+        if(low == high){
+            seg[idx] = arr[low];
+            return;
         }
-        cur->isEnd = true;
+        int mid = low + (high-low)/2;
+        build(2*idx+1, low, mid);
+        build(2*idx+2, mid+1, high);
+        {
+            seg[idx] = min(seg[2*idx+1], seg[2*idx+2]);
+        }
     }
-    
+    int query(int idx, int low, int high, int l, int r){  //O(logN)
+        if(l<= low and high <= r) return seg[idx];
+        {
+            if(high < l or low > r) {
+                return 1e9;
+            }
+        }
+        int mid = low + (high-low)/2;
+        int lval = query(2*idx+1, low, mid, l, r);
+        int rval = query(2*idx+2, mid +1, high, l, r);
+        {
+            return min(lval, rval);
+        }
+    }
+    void update(int idx, int low, int high, int i, int val){  // O(logN)
+        if(low == high){
+            if(val < arr[i])
+                seg[idx] = arr[i] = val;
+            return;
+        }
+        int mid = low + (high - low)/2;
+        if(i <= mid){
+            update(2*idx+1, low, mid, i, val);
+        }else{
+            update(2*idx+2, mid+1, high, i, val);
+        }
+        {
+            seg[idx] = min(seg[2*idx+1], seg[2*idx+2]);
+        }
+    }
 };
 
 class Solution {
+    void zValues(string& s, vector<int> &z) {
+        int n = ( int )s.length();
+        for (int i = 1, l = 0, r = 0; i < n; ++i) {
+            if (i <= r) z[i] = min(r - i + 1, z[i - l]);
+            while (i + z[i] < n && s[z[i]] == s[i + z[i]]) ++z[i];
+            if (i + z[i] - 1 > r) l = i, r = i + z[i] - 1;
+        }
+        return;
+    }
 public:
     int minValidStrings(vector<string>& words, string target) {
         int n = target.size();
         
-        Trie *tr = new Trie();
-        for(const string &s: words)
-            tr->add(s);
-        
-        vector<int> dp(n+1, 1e9);
-        
-        dp[0] = 0;
-        for(int i = 0; i<n; i++){
-            TrieNode *cur = tr->root;
-            for(int j = i+1; j<=n; j++){
-                int idx = target[j-1] - 'a';
-                if(cur->children[idx] != NULL){
-                    cur = cur->children[idx];
-                    dp[j] = min(dp[j], dp[i] + 1);
-                }else break;
+        vector<int> reach(n, -1);
+        for(string &s: words){
+            string temp = s + "$" + target;
+            vector<int> z(temp.size());
+            zValues(temp, z);
+            
+            for(int i = 0; i<n; i++){
+                reach[i] = max(reach[i], i + z[s.size() + 1 + i]);
             }
         }
         
-        int ans = dp[n];
-        if(ans == 1e9) return -1;
-        return ans;
+        vector<int> dp(n+1, 1e9);
+        dp[n] = 0;
+        segTree st(dp, n+1);
+        st.build(0, 0, n);
+        
+        for(int i = n-1; i>=0; i--){
+            int end = reach[i];
+            
+            st.update(0, 0, n, i, st.query(0, 0, n, i, end) + 1);
+        }
+        int ans = st.query(0, 0, n, 0, 0);
+        return ans==1e9?-1:ans;
     }
 };
